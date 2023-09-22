08ed492bf9e5f7247df932c42d8833f4924994e3
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-09-22 16:17:06 +0100

    whole title

--- integration_tests/pages/courtCaseNextCourtDateQuestionPage.ts
+++ integration_tests/pages/courtCaseNextCourtDateQuestionPage.ts
@@ -2,6 +2,6 @@ import Page from './page'
 
 export default class CourtCaseNextCourtDateQuestionPage extends Page {
   constructor() {
-    super('Do you know the next court date')
+    super('Do you know the next court date?')
   }
 }

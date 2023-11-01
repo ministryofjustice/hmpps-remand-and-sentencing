c48314a9397c1ab985ac38bc32cf54b0bf43841a
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2023-11-01 11:18:45 +0000

    RASS-32 updated prisoner details block (#53)
    * RASS-32 updated prisoner details block
    
    * added pnc number fallback

--- server/middleware/setUpPrisonerDetails.ts
+++ server/middleware/setUpPrisonerDetails.ts
@@ -1,13 +1,15 @@
 import { RequestHandler } from 'express'
 import logger from '../../logger'
 import PrisonerService from '../services/prisonerService'
+import PrisonerDetailsModel from '../routes/data/PrisonerDetailsModel'
 
 export default function setupPrisonerDetails(prisonerService: PrisonerService): RequestHandler {
   return async (req, res, next) => {
     const { nomsId } = req.params
     try {
       const { token } = res.locals.user
-      res.locals.prisonerDetails = await prisonerService.getPrisonerDetails(nomsId, token)
+      const prisonApiPrisoner = await prisonerService.getPrisonerDetails(nomsId, token)
+      res.locals.prisonerDetails = new PrisonerDetailsModel(prisonApiPrisoner)
       next()
     } catch (error) {
       logger.error(error, `Failed to retrieve prisoner details for: ${nomsId}`)
---
+++ server/routes/data/PrisonerDetailsModel.ts
@@ -0,0 +1,36 @@
+import dayjs from 'dayjs'
+import { PrisonApiPrisoner } from '../../@types/prisonApi/prisonClientTypes'
+
+export default class PrisonerDetailsModel {
+  offenderNo: string
+
+  firstName: string
+
+  lastName: string
+
+  establishment: string
+
+  cellNumber: string
+
+  dateOfBirth: string
+
+  pncNumber: string
+
+  status: string
+
+  constructor(prisonApiPrisoner: PrisonApiPrisoner) {
+    this.offenderNo = prisonApiPrisoner.offenderNo
+    this.firstName = prisonApiPrisoner.firstName
+    this.lastName = prisonApiPrisoner.lastName
+    this.establishment = prisonApiPrisoner.assignedLivingUnit.agencyName
+    this.cellNumber = prisonApiPrisoner.assignedLivingUnit.description
+    this.dateOfBirth = dayjs(prisonApiPrisoner.dateOfBirth).format('DD/MM/YYYY')
+    const prisonerPNCNumber = prisonApiPrisoner.identifiers.find(identifier => identifier.type === 'PNC')
+    if (prisonerPNCNumber) {
+      this.pncNumber = prisonerPNCNumber.value
+    } else {
+      this.pncNumber = 'Not available'
+    }
+    this.status = prisonApiPrisoner.legalStatus
+  }
+}

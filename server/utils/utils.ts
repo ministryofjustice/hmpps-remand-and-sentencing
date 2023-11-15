b39ad6e141e4cb94b519a1f70ec79428e719826e
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-11-15 09:24:04 +0000

    clean up

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -1,7 +1,4 @@
 import dayjs from 'dayjs'
-import objectSupport from 'dayjs/plugin/objectSupport'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
 import config from '../config'
 
 const properCase = (word: string): string =>

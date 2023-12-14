3172e1082122bb4c4ffabd123cc6a1b284407b69
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-12-14 12:30:58 +0000

    RASS-99 integrate with create court case

--- server/@types/remandAndSentencingApi/remandAndSentencingClientTypes.ts
+++ server/@types/remandAndSentencingApi/remandAndSentencingClientTypes.ts
@@ -1,3 +1,13 @@
 import { components } from './index'
 
 export type RemandAndSentencingPerson = components['schemas']['PersonDetails']
+
+export type CreateCourtCase = components['schemas']['CreateCourtCase']
+
+export type CreateCourtAppearance = components['schemas']['CreateCourtAppearance']
+
+export type CreateNextCourtAppearance = components['schemas']['CreateNextCourtAppearance']
+
+export type CreateCharge = components['schemas']['CreateCharge']
+
+export type CreateCourtCaseResponse = components['schemas']['CreateCourtCaseResponse']
--- server/services/remandAndSentencingService.ts
+++ server/services/remandAndSentencingService.ts
@@ -1,8 +1,51 @@
-import type { RemandAndSentencingPerson } from '../@types/remandAndSentencingApi/remandAndSentencingClientTypes'
+import type { CourtCase } from 'models'
+import dayjs from 'dayjs'
+import type {
+  CreateCharge,
+  CreateCourtAppearance,
+  CreateCourtCase,
+  CreateCourtCaseResponse,
+  CreateNextCourtAppearance,
+  RemandAndSentencingPerson,
+} from '../@types/remandAndSentencingApi/remandAndSentencingClientTypes'
 import RemandAndSentencingApiClient from '../api/remandAndSentencingApiClient'
 
 export default class RemandAndSentencingService {
   async getPersonDetails(prisonerId: string, token: string): Promise<RemandAndSentencingPerson> {
     return new RemandAndSentencingApiClient(token).getPersonDetail(prisonerId)
+  }
+
+  async createCourtCase(prisonerId: string, token: string, courtCase: CourtCase): Promise<CreateCourtCaseResponse> {
+    const appearances = courtCase.appearances.map(courtAppearance => {
+      let nextCourtAppearance
+      if (courtAppearance.nextHearingCourtSelect) {
+        nextCourtAppearance = {
+          appearanceDate: dayjs(courtAppearance.nextHearingDate).format('YYYY-MM-DD'),
+          courtCode: courtAppearance.nextHearingCourtName,
+          appearanceType: courtAppearance.nextHearingType,
+        } as CreateNextCourtAppearance
+      }
+      const charges = courtAppearance.offences.map(offence => {
+        return {
+          offenceCode: offence.offenceCode,
+          offenceStartDate: dayjs(offence.offenceStartDate).format('YYYY-MM-DD'),
+          outcome: offence.outcome,
+          ...(offence.offenceEndDate && { offenceEndDate: dayjs(offence.offenceEndDate).format('YYYY-MM-DD') }),
+        } as CreateCharge
+      })
+      return {
+        outcome: courtAppearance.overallCaseOutcome,
+        courtCode: courtAppearance.courtName,
+        courtCaseReference: courtAppearance.caseReferenceNumber,
+        appearanceDate: dayjs(courtAppearance.warrantDate).format('YYYY-MM-DD'),
+        nextCourtAppearance,
+        charges,
+      } as CreateCourtAppearance
+    })
+    const createCourtCase = {
+      prisonerId,
+      appearances,
+    } as CreateCourtCase
+    return new RemandAndSentencingApiClient(token).createCourtCase(createCourtCase)
   }
 }

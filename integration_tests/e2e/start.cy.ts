6d30a13c8f3cbf518422ecfd60fc40b0cc80a338
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-01-10 09:19:45 +0000

    fixed assertion

--- integration_tests/e2e/start.cy.ts
+++ integration_tests/e2e/start.cy.ts
@@ -35,7 +35,7 @@ context('Start Page', () => {
 
   it('displays court case summary', () => {
     startPage.courtCaseSummaryList('3fa85f64-5717-4562-b3fc-2c963f66afa6').getSummaryList().should('deep.equal', {
-      'Case references': 'C894623',
+      'Case references': 'C894623, F23325',
       'Overall case outcome': 'Remand in Custody (Bail Refused)',
       'Next hearing': 'Birmingham Crown Court Court appearance 15 12 2024',
     })
@@ -47,17 +47,17 @@ context('Start Page', () => {
       .getTable()
       .should('deep.equal', [
         {
-          'Case reference': 'F23325',
-          Location: 'Birmingham Crown Court',
-          'Warrant date': '15 10 2022',
-          Outcome: 'Sentence Postponed',
-          '': 'View and edit',
-        },
-        {
           'Case reference': 'C894623',
           Location: 'Birmingham Crown Court',
           'Warrant date': '15 12 2023',
           Outcome: 'Remand in Custody (Bail Refused)',
+          '': 'View and edit',
+        },
+        {
+          'Case reference': 'F23325',
+          Location: 'Birmingham Crown Court',
+          'Warrant date': '15 10 2022',
+          Outcome: 'Sentence Postponed',
           '': 'View and edit',
         },
       ])

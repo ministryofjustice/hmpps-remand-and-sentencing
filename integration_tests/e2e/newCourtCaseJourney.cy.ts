5225af6b17e508980db40ef69fafa03f415d7055
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-01-16 13:29:26 +0000

    set up e2e test for sentencing journey

--- integration_tests/e2e/newCourtCaseJourney.cy.ts
+++ integration_tests/e2e/newCourtCaseJourney.cy.ts
@@ -26,6 +26,7 @@ context('New Court Case journey', () => {
     cy.task('stubGetPersonDetails')
     cy.task('stubGetOffenceByCode')
     cy.task('stubCreateCourtCase')
+    cy.task('stubCreateSentenceCourtCase')
     cy.task('stubSearchCourtCases', {})
     cy.task('stubGetOffencesByCodes')
     cy.task('stubUploadWarrant')
@@ -33,7 +34,7 @@ context('New Court Case journey', () => {
     cy.visit('/person/A1234AB')
   })
 
-  it('fill in whole journey and check answers shows inputted values', () => {
+  it('fill in remand journey', () => {
     const startPage = Page.verifyOnPage(StartPage)
     startPage.actionListLink().click()
     const courtCaseReferencePage = Page.verifyOnPage(CourtCaseReferencePage)
@@ -102,6 +103,96 @@ context('New Court Case journey', () => {
     const courtCaseNextHearingAnswersPage = Page.verifyOnPage(CourtCaseCheckNextHearingAnswersPage)
     courtCaseNextHearingAnswersPage.button().click()
     cy.task('verifyCreateCourtCaseRequest').should('equal', 1)
+    // once confirmation page is implemented it will go to that page
+  })
+
+  it('fill in sentencing journey', () => {
+    const startPage = Page.verifyOnPage(StartPage)
+    startPage.actionListLink().click()
+    const courtCaseReferencePage = Page.verifyOnPage(CourtCaseReferencePage)
+    courtCaseReferencePage.input().type('1234')
+    courtCaseReferencePage.button().click()
+    const courtCaseWarrantDatePage = Page.verifyOnPage(CourtCaseWarrantDatePage)
+    courtCaseWarrantDatePage.dayDateInput('warrant-date').type('12')
+    courtCaseWarrantDatePage.monthDateInput('warrant-date').type('5')
+    courtCaseWarrantDatePage.yearDateInput('warrant-date').type('2023')
+    courtCaseWarrantDatePage.button().click()
+    const courtCaseCourtNamePage = Page.verifyOnPage(CourtCaseCourtNamePage)
+    courtCaseCourtNamePage.autoCompleteInput().type('cou')
+    courtCaseCourtNamePage.firstAutoCompleteOption().click()
+    courtCaseCourtNamePage.button().click()
+
+    const courtCaseWarrantTypePage = Page.verifyOnPage(CourtCaseWarrantTypePage)
+    courtCaseWarrantTypePage.radioSelector('SENTENCING').click()
+    courtCaseWarrantTypePage.button().click()
+
+    const courtCaseWarrantUploadPage = Page.verifyOnPage(CourtCaseWarrantUploadPage)
+    courtCaseWarrantUploadPage.fileInput().selectFile('integration_tests/resources/aWarrant.jpg')
+    courtCaseWarrantUploadPage.button().click()
+
+    const courtCaseOverallCaseOutcomePage = Page.verifyOnPage(CourtCaseOverallCaseOutcomePage)
+    courtCaseOverallCaseOutcomePage.radioSelector('Sentencing outcome 1').click()
+    courtCaseOverallCaseOutcomePage.button().click()
+
+    const courtCaseCaseOutcomeAppliedAllPage = Page.verifyOnPage(CourtCaseCaseOutcomeAppliedAllPage)
+    courtCaseCaseOutcomeAppliedAllPage.radioSelector('true').click()
+    courtCaseCaseOutcomeAppliedAllPage.button().click()
+
+    // this is where tagged bail would appear
+
+    const courtCaseCheckAnswersPage = Page.verifyOnPage(CourtCaseCheckAnswersPage)
+    courtCaseCheckAnswersPage.summaryList().getSummaryList().should('deep.equal', {
+      'Court case reference number': '1234',
+      'Court name': 'Bradford Crown Court',
+      'Warrant date': '12 05 2023',
+      'Overall case outcome': 'Sentencing outcome 1',
+    })
+
+    // this is where count number would appear
+
+    courtCaseCheckAnswersPage.button().click()
+    const offenceOffenceCodePage = Page.verifyOnPage(OffenceOffenceCodePage)
+    offenceOffenceCodePage.input().type('PS90037')
+    offenceOffenceCodePage.button().click()
+    const offenceOffenceCodeConfirmPage = Page.verifyOnPage(OffenceOffenceCodeConfirmPage)
+    offenceOffenceCodeConfirmPage.button().click()
+
+    // this is where terror related would appear
+
+    const offenceOffenceDatePage = Page.verifyOnPage(OffenceOffenceDatePage)
+    offenceOffenceDatePage.dayDateInput('offence-start-date').type('12')
+    offenceOffenceDatePage.monthDateInput('offence-start-date').type('5')
+    offenceOffenceDatePage.yearDateInput('offence-start-date').type('2023')
+    offenceOffenceDatePage.button().click()
+
+    // this is where sentence type would appear
+
+    // this is where sentence length would appear
+
+    const offenceCheckOffenceAnswersPage = new OffenceCheckOffenceAnswersPage(1, '1234')
+    offenceCheckOffenceAnswersPage.finishAddingButton().click()
+
+    // this is where consecutive sentences would appear
+
+    // there would be no next hearing journey
+
+    const courtCaseNextHearingSetPage = Page.verifyOnPage(CourtCaseNextHearingSetPage)
+    courtCaseNextHearingSetPage.radioSelector('true').click()
+    courtCaseNextHearingSetPage.button().click()
+    const courtCaseNextHearingTypePage = Page.verifyOnPage(CourtCaseNextHearingTypePage)
+    courtCaseNextHearingTypePage.radioSelector('Court appearance').click()
+    courtCaseNextHearingTypePage.button().click()
+    const courtCaseNextHearingDatePage = Page.verifyOnPage(CourtCaseNextHearingDatePage)
+    courtCaseNextHearingDatePage.dayDateInput('next-hearing-date').type('18')
+    courtCaseNextHearingDatePage.monthDateInput('next-hearing-date').type('10')
+    courtCaseNextHearingDatePage.yearDateInput('next-hearing-date').type('2023')
+    courtCaseNextHearingDatePage.button().click()
+    const courtCaseNextHearingCourtSetPage = Page.verifyOnPage(CourtCaseNextHearingCourtSetPage)
+    courtCaseNextHearingCourtSetPage.radioSelector('true').click()
+    courtCaseNextHearingCourtSetPage.button().click()
+    const courtCaseNextHearingAnswersPage = Page.verifyOnPage(CourtCaseCheckNextHearingAnswersPage)
+    courtCaseNextHearingAnswersPage.button().click()
+    cy.task('verifyCreateSentenceCourtCaseRequest').should('equal', 1)
     // once confirmation page is implemented it will go to that page
   })
 })

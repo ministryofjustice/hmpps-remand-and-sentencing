46afa6ac83094060e838ce7688b842f29b4cba86
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-04-15 15:56:25 +0100

    fix submitting tagged bail

--- server/validation/minWholeNumberRule.ts
+++ server/validation/minWholeNumberRule.ts
@@ -1,5 +1,5 @@
 export default function validate(value, options): boolean {
   const valueNumber = Number(value)
   const optionsNumber = Number(options)
-  return optionsNumber <= valueNumber && Number.isInteger(value)
+  return optionsNumber <= valueNumber && Number.isInteger(valueNumber)
 }

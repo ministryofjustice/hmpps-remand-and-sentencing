1ff098c92bcbccd4c3cd8f8af31dcfff1b330999
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-04-26 10:12:10 +0100

    RASS-222 updated layout and validation to next hearing date

--- integration_tests/e2e/courtCaseNextHearingDate.cy.ts
+++ integration_tests/e2e/courtCaseNextHearingDate.cy.ts
@@ -23,4 +23,27 @@ context('Next hearing date page', () => {
   it('button to continue is displayed', () => {
     courtCaseNextHearingDatePage.button().should('contain.text', 'Continue')
   })
+
+  it('submitting without entering anything in the inputs results in an error', () => {
+    courtCaseNextHearingDatePage.button().click()
+    courtCaseNextHearingDatePage
+      .errorSummary()
+      .trimTextContent()
+      .should(
+        'equal',
+        'There is a problem Next court date must include year Next court date must include month Next court date must include day',
+      )
+  })
+
+  it('submitting an invalid format time results in an error', () => {
+    courtCaseNextHearingDatePage.dayDateInput('nextHearingDate').type('15')
+    courtCaseNextHearingDatePage.monthDateInput('nextHearingDate').type('7')
+    courtCaseNextHearingDatePage.yearDateInput('nextHearingDate').type('2026')
+    courtCaseNextHearingDatePage.nextHearingTimeInput().type('123:456')
+    courtCaseNextHearingDatePage.button().click()
+    courtCaseNextHearingDatePage
+      .errorSummary()
+      .trimTextContent()
+      .should('equal', 'There is a problem Time must be in 1:00 or 13:00 format')
+  })
 })

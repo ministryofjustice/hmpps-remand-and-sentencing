457dd4c712eb491eee6cdc4fc590ec13a2f32b6f
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-05-02 14:52:36 +0100

    fixes

--- server/validation/requireSentenceLength.ts
+++ server/validation/requireSentenceLength.ts
@@ -3,5 +3,7 @@ export default function validate(value: string): boolean {
     return true
   }
   const form = this.validator.input
-  return Object.values(form).some(formValue => formValue)
+  return Object.entries(form)
+    .filter(([key]) => key.startsWith('sentenceLength-'))
+    .some(([_, formValue]) => formValue)
 }

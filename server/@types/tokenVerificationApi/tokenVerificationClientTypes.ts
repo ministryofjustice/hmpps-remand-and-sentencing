530b1269ee2b7bd364d9ff63118897ed975f2e91
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-07-03 11:25:57 +0100

    RASS-269 migrated token verification api call into api client

---
+++ server/@types/tokenVerificationApi/tokenVerificationClientTypes.ts
@@ -0,0 +1,3 @@
+import { components } from './index'
+
+export type TokenDto = components['schemas']['TokenDto']
--- server/data/tokenVerification.ts
+++ server/data/tokenVerification.ts
@@ -1,23 +1,7 @@
-import superagent from 'superagent'
 import type { Request } from 'express'
-import getSanitisedError from '../sanitisedError'
 import config from '../config'
 import logger from '../../logger'
-
-function getApiClientToken(token: string) {
-  return superagent
-    .post(`${config.apis.tokenVerification.url}/token/verify`)
-    .auth(token, { type: 'bearer' })
-    .timeout(config.apis.tokenVerification.timeout)
-    .retry(2, (err, _) => {
-      if (err) logger.info(`Retry handler found API error with ${err.code} ${err.message}`)
-      return undefined // retry handler only for logging retries, not to influence retry logic
-    })
-    .then(response => Boolean(response.body && response.body.active))
-    .catch(error => {
-      logger.error(getSanitisedError(error), 'Error calling tokenVerificationApi')
-    })
-}
+import TokenVerificationApiClient from '../api/tokenVerificationApiClient'
 
 export type TokenVerifier = (request: Request) => Promise<boolean | void>
 
@@ -35,7 +19,7 @@ const tokenVerifier: TokenVerifier = async request => {
 
   logger.debug(`token request for user "${user.username}'`)
 
-  const result = await getApiClientToken(user.token)
+  const result = await new TokenVerificationApiClient(user.token).verifyToken()
   if (result) {
     request.verified = true
   }

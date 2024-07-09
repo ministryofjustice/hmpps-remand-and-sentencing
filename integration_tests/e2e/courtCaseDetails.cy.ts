feb7fed229c62ca557b311178366def697d91f9b
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-07-09 16:06:33 +0100

    removed commented out code

--- integration_tests/e2e/courtCaseDetails.cy.ts
+++ integration_tests/e2e/courtCaseDetails.cy.ts
@@ -37,28 +37,4 @@ context('Court Case details Page', () => {
       })
     })
   })
-
-  // context('sentence appearance', () => {
-  //   beforeEach(() => {
-  //     cy.task('stubGetSentenceAppearanceDetails')
-  //     cy.signIn()
-  //     cy.visit(
-  //       '/person/A1234AB/edit-court-case/83517113-5c14-4628-9133-1e3cb12e31fa/edit-court-appearance/3fa85f64-5717-4562-b3fc-2c963f66afa6/details',
-  //     )
-  //     courtCaseDetailsPage = Page.verifyOnPageTitle(
-  //       CourtCaseDetailsPage,
-  //       'Edit appearance C894623 at Birmingham Crown Court on 15 12 2023',
-  //     )
-  //   })
-
-  //   it('appearance summary shows correct data', () => {
-  //     courtCaseDetailsPage.appearanceSummaryList().getSummaryList().should('deep.equal', {
-  //       'Case reference': 'C894623',
-  //       'Warrant date': '15 12 2023',
-  //       'Court name': 'Birmingham Crown Court',
-  //       'Overall case outcome': 'Imprisonment',
-  //       'Overall sentence length': '4 years',
-  //     })
-  //   })
-  // })
 })

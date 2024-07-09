6fa2e90a33686cf9172080d67aabe37d48c705af
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2024-07-09 16:29:29 +0100

    RASS-248 first part of court case details page (#334)
    * RASS-248 first part of court case details page
    
    * removed commented out code

---
+++ server/routes/data/CourtCasesDetailsModel.ts
@@ -0,0 +1,74 @@
+import dayjs from 'dayjs'
+import type { Offence } from 'models'
+import {
+  PageCourtCaseAppearance,
+  PageCourtCaseContent,
+} from '../../@types/remandAndSentencingApi/remandAndSentencingClientTypes'
+import config from '../../config'
+import { chargeToOffence } from '../../utils/mappingUtils'
+
+export default class CourtCasesDetailsModel {
+  courtCaseUuid: string
+
+  caseReferences: string
+
+  overallCaseOutcome: string
+
+  nextHearing: string[]
+
+  appearanceTotal: number
+
+  showingAppearanceTotal?: number
+
+  latestAppearances: PageCourtCaseAppearance[]
+
+  chargeTotal: number
+
+  showingChargeTotal?: number
+
+  offences: Offence[]
+
+  latestCourtName: string
+
+  latestAppearanceDate: string
+
+  constructor(pageCourtCaseContent: PageCourtCaseContent) {
+    this.courtCaseUuid = pageCourtCaseContent.courtCaseUuid
+    this.caseReferences = Array.from(
+      new Set(pageCourtCaseContent.appearances.map(appearance => appearance.courtCaseReference)),
+    ).join(', ')
+    this.overallCaseOutcome = pageCourtCaseContent.latestAppearance.outcome
+
+    if (pageCourtCaseContent.latestAppearance.nextCourtAppearance) {
+      const appearanceDate = dayjs(pageCourtCaseContent.latestAppearance.nextCourtAppearance.appearanceDate)
+      let appearanceDateFormatted = appearanceDate.format(config.dateFormat)
+      if (pageCourtCaseContent.latestAppearance.nextCourtAppearance.appearanceTime) {
+        appearanceDateFormatted = appearanceDate.format(config.dateTimeFormat)
+      }
+      this.nextHearing = [
+        pageCourtCaseContent.latestAppearance.nextCourtAppearance.courtCode,
+        pageCourtCaseContent.latestAppearance.nextCourtAppearance.appearanceType,
+        appearanceDateFormatted,
+      ]
+    } else {
+      this.nextHearing = ['Date to be fixed']
+    }
+    this.appearanceTotal = pageCourtCaseContent.appearances.length
+    if (this.appearanceTotal > 5) {
+      this.showingAppearanceTotal = 5
+    }
+    this.latestAppearances = pageCourtCaseContent.appearances
+      .sort((a, b) => (dayjs(a.appearanceDate).isBefore(dayjs(b.appearanceDate)) ? 1 : -1))
+      .slice(0, 5)
+    this.chargeTotal = pageCourtCaseContent.latestAppearance.charges.length
+    if (this.chargeTotal > 5) {
+      this.showingChargeTotal = 5
+    }
+    this.offences = pageCourtCaseContent.latestAppearance.charges
+      .sort((a, b) => (dayjs(a.offenceStartDate).isBefore(dayjs(b.offenceStartDate)) ? 1 : -1))
+      .map(charge => chargeToOffence(charge))
+      .slice(0, 5)
+    this.latestCourtName = pageCourtCaseContent.latestAppearance.courtCode
+    this.latestAppearanceDate = dayjs(pageCourtCaseContent.latestAppearance.appearanceDate).format(config.dateFormat)
+  }
+}

f50fc9a24b6312b1c43a2bac6f6c5b34fc09287e
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-07-25 16:36:29 +0100

    update caption with appearance information

--- server/middleware/setUpCurrentCourtAppearance.ts
+++ server/middleware/setUpCurrentCourtAppearance.ts
@@ -3,11 +3,12 @@ import CourtAppearanceService from '../services/courtAppearanceService'
 
 export default function setupCurrentCourtAppearance(courtAppearanceService: CourtAppearanceService): RequestHandler {
   return async (req, res, next) => {
-    const { nomsId } = req.params
+    const { nomsId, addOrEditCourtAppearance } = req.params
     const courtAppearance = courtAppearanceService.getSessionCourtAppearance(req.session, nomsId)
     res.locals.courtAppearance = courtAppearance
     res.locals.offences = courtAppearance.offences.filter(offence => !offence.sentence)
     res.locals.sentences = courtAppearance.offences.filter(offence => offence.sentence)
+    res.locals.isAddCourtAppearance = addOrEditCourtAppearance === 'add-court-appearance'
     next()
   }
 }

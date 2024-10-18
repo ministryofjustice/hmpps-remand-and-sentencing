e027a3d17dca1ed575c22c0527bab0e59da81261
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-10-18 10:13:17 +0100

    RASS-279 update confirm offence code page

--- integration_tests/pages/offenceOffenceCodeConfirmPage.ts
+++ integration_tests/pages/offenceOffenceCodeConfirmPage.ts
@@ -1,7 +1,9 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class OffenceOffenceCodeConfirmPage extends Page {
   constructor() {
     super('Confirm the offence')
   }
+
+  offenceSummaryList = (): PageElement => cy.get('[data-qa=offenceSummaryList]')
 }

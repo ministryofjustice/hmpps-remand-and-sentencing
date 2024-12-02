6fc54dda0c0c290bf94df663b125ef232f6a46b2
Dan Benton <daniel.benton@digital.justice.gov.uk>
2024-12-02 17:23:44 +0000

    Ignore esbuild js files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
 export default hmppsConfig({
-  extraIgnorePaths: ['assets/js/*.js'],
+  extraIgnorePaths: ['assets/js/*.js', 'esbuild/*.js'],
 })

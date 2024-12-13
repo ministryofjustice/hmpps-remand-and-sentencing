832d78d1840123e8439acaa5e6ab9fa7fa1140ae
Dan Benton <daniel.benton@digital.justice.gov.uk>
2024-12-13 08:57:16 +0000

    RASS-430 Remove unused import

--- server/utils/mappingUtils.ts
+++ server/utils/mappingUtils.ts
@@ -11,7 +11,6 @@ import {
   CreatePeriodLength,
   CreateSentence,
   DraftCourtAppearance,
-  DraftCreateCourtAppearance,
   DraftCreateCourtCase,
   NextCourtAppearance,
   PageCourtCaseAppearance,

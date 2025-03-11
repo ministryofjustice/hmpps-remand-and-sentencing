5f79abfb805e094684d554dae4d79c1a98207453
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2025-03-11 12:46:17 +0000

    RASS-843 retrieve next hearing when set is true

--- integration_tests/e2e/courtCaseCheckNextHearingAnswers.cy.ts
+++ integration_tests/e2e/courtCaseCheckNextHearingAnswers.cy.ts
@@ -6,19 +6,45 @@ import CourtCaseNextHearingTypePage from '../pages/courtCaseNextHearingTypePage'
 import CourtCaseNextHearingDatePage from '../pages/courtCaseNextHearingDatePage'
 import Page from '../pages/page'
 import CourtCaseCourtNamePage from '../pages/courtCaseCourtNamePage'
+import CourtCaseNextHearingSetPage from '../pages/courtCaseNextHearingSetPage'
 
 context('Check Next Hearing Answers page', () => {
-  let courtCaseCheckOffenceAnswersPage: CourtCaseCheckNextHearingAnswersPage
+  let courtCaseNextHearingAnswersPage: CourtCaseCheckNextHearingAnswersPage
+  const futureDate = dayjs().add(10, 'day')
   beforeEach(() => {
     cy.task('happyPathStubs')
     cy.task('stubGetAppearanceTypeByUuid')
+    cy.task('stubGetCourtById', {})
     cy.signIn()
-    cy.visit('/person/A1234AB/add-court-case/0/add-court-appearance/0/check-next-hearing-answers')
-    courtCaseCheckOffenceAnswersPage = Page.verifyOnPage(CourtCaseCheckNextHearingAnswersPage)
+    cy.visit('/person/A1234AB/add-court-case/0/add-court-appearance/0/court-name')
+    const courtCaseCourtNamePage = Page.verifyOnPageTitle(CourtCaseCourtNamePage, 'What is the court name?')
+    courtCaseCourtNamePage.autoCompleteInput().type('cou')
+    courtCaseCourtNamePage.firstAutoCompleteOption().contains('Accrington Youth Court')
+    courtCaseCourtNamePage.firstAutoCompleteOption().click()
+    courtCaseCourtNamePage.continueButton().click()
+    cy.visit('/person/A1234AB/add-court-case/0/add-court-appearance/0/next-hearing-select')
+    const courtCaseNextHearingSetPage = Page.verifyOnPage(CourtCaseNextHearingSetPage)
+    courtCaseNextHearingSetPage.radioLabelSelector('true').click()
+    courtCaseNextHearingSetPage.continueButton().click()
+    const courtCaseNextHearingTypePage = Page.verifyOnPage(CourtCaseNextHearingTypePage)
+    courtCaseNextHearingTypePage.radioLabelContains('Court appearance').click()
+    courtCaseNextHearingTypePage.continueButton().click()
+
+    const courtCaseNextHearingDatePage = Page.verifyOnPage(CourtCaseNextHearingDatePage)
+
+    courtCaseNextHearingDatePage.dayDateInput('nextHearingDate').type(futureDate.date().toString())
+    courtCaseNextHearingDatePage.monthDateInput('nextHearingDate').type((futureDate.month() + 1).toString())
+    courtCaseNextHearingDatePage.yearDateInput('nextHearingDate').type(futureDate.year().toString())
+    courtCaseNextHearingDatePage.continueButton().click()
+
+    const courtCaseNextHearingCourtSetPage = Page.verifyOnPage(CourtCaseNextHearingCourtSetPage)
+    courtCaseNextHearingCourtSetPage.radioLabelSelector('true').click()
+    courtCaseNextHearingCourtSetPage.continueButton().click()
+    courtCaseNextHearingAnswersPage = Page.verifyOnPage(CourtCaseCheckNextHearingAnswersPage)
   })
 
   it('displays person details', () => {
-    courtCaseCheckOffenceAnswersPage
+    courtCaseNextHearingAnswersPage
       .prisonerBanner()
       .should('contain.text', 'Haggler, Marvin')
       .and('contain.text', 'A1234AB')
@@ -27,11 +53,11 @@ context('Check Next Hearing Answers page', () => {
   })
 
   it('button to continue is displayed', () => {
-    courtCaseCheckOffenceAnswersPage.continueButton().should('contain.text', 'Accept and continue')
+    courtCaseNextHearingAnswersPage.continueButton().should('contain.text', 'Accept and continue')
   })
 
   it('clicking next hearing type and submitting goes back to check answers page', () => {
-    courtCaseCheckOffenceAnswersPage.changeLink('A1234AB', '0', '0', 'next-hearing-type').click()
+    courtCaseNextHearingAnswersPage.changeLink('A1234AB', '0', '0', 'next-hearing-type').click()
     const courtCaseNextHearingTypePage = Page.verifyOnPage(CourtCaseNextHearingTypePage)
     courtCaseNextHearingTypePage.radioLabelContains('Court appearance').click()
     courtCaseNextHearingTypePage.continueButton().click()
@@ -39,32 +65,39 @@ context('Check Next Hearing Answers page', () => {
   })
 
   it('clicking next hearing location and submitting goes back to check answers page', () => {
-    cy.task('stubGetCourtById', {})
-    cy.visit('/person/A1234AB/add-court-case/0/add-court-appearance/0/court-name')
-    const courtCaseCourtNamePage = Page.verifyOnPageTitle(CourtCaseCourtNamePage, 'What is the court name?')
-    courtCaseCourtNamePage.autoCompleteInput().type('cou')
-    courtCaseCourtNamePage.firstAutoCompleteOption().contains('Accrington Youth Court')
-    courtCaseCourtNamePage.firstAutoCompleteOption().click()
-    courtCaseCourtNamePage.continueButton().click()
-    cy.visit('/person/A1234AB/add-court-case/0/add-court-appearance/0/check-next-hearing-answers')
-    courtCaseCheckOffenceAnswersPage.changeLink('A1234AB', '0', '0', 'next-hearing-court-select').click()
+    cy.task('stubGetCourtById', {
+      courtId: 'STHHPM',
+      courtName: 'Southampton Magistrate Court',
+    })
+    courtCaseNextHearingAnswersPage.changeLink('A1234AB', '0', '0', 'next-hearing-court-select').click()
     const courtCaseNextHearingCourtSetPage = Page.verifyOnPage(CourtCaseNextHearingCourtSetPage)
     courtCaseNextHearingCourtSetPage.radioLabelSelector('false').click()
     courtCaseNextHearingCourtSetPage.continueButton().click()
     const courtCaseNextHearingCourtNamePage = Page.verifyOnPage(CourtCaseNextHearingCourtNamePage)
     courtCaseNextHearingCourtNamePage.autoCompleteInput().type('cou')
-    courtCaseNextHearingCourtNamePage.firstAutoCompleteOption().click()
+    courtCaseNextHearingCourtNamePage.secondAutoCompleteOption().contains('Southampton Magistrate Court').click()
     courtCaseNextHearingCourtNamePage.continueButton().click()
-    Page.verifyOnPage(CourtCaseCheckNextHearingAnswersPage)
+    courtCaseNextHearingAnswersPage = Page.verifyOnPage(CourtCaseCheckNextHearingAnswersPage)
+    courtCaseNextHearingAnswersPage
+      .summaryList()
+      .getSummaryList()
+      .should('deep.equal', {
+        'Next hearing date': futureDate.format('DD/MM/YYYY'),
+        'Next hearing location': 'Southampton Magistrate Court',
+        'Next hearing type': 'Court appearance',
+      })
   })
 
   it('clicking next hearing date and submitting goes back to check answers page', () => {
-    courtCaseCheckOffenceAnswersPage.changeLink('A1234AB', '0', '0', 'next-hearing-date').click()
+    courtCaseNextHearingAnswersPage.changeLink('A1234AB', '0', '0', 'next-hearing-date').click()
     const courtCaseNextHearingDatePage = Page.verifyOnPage(CourtCaseNextHearingDatePage)
-    const futureDate = dayjs().add(10, 'day')
-    courtCaseNextHearingDatePage.dayDateInput('nextHearingDate').type(futureDate.date().toString())
-    courtCaseNextHearingDatePage.monthDateInput('nextHearingDate').type((futureDate.month() + 1).toString())
-    courtCaseNextHearingDatePage.yearDateInput('nextHearingDate').type(futureDate.year().toString())
+    const differentFutureDate = futureDate.add(10, 'days')
+    courtCaseNextHearingDatePage.dayDateInput('nextHearingDate').clear().type(differentFutureDate.date().toString())
+    courtCaseNextHearingDatePage
+      .monthDateInput('nextHearingDate')
+      .clear()
+      .type((differentFutureDate.month() + 1).toString())
+    courtCaseNextHearingDatePage.yearDateInput('nextHearingDate').clear().type(differentFutureDate.year().toString())
     courtCaseNextHearingDatePage.continueButton().click()
     Page.verifyOnPage(CourtCaseCheckNextHearingAnswersPage)
   })

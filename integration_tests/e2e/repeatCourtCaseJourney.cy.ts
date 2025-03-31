0da36ba43871c655444a48a7044ae77ab6a0a313
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2025-03-31 14:42:16 +0100

    RASS-915 clean up end to end tests

--- integration_tests/e2e/repeatCourtCaseJourney.cy.ts
+++ integration_tests/e2e/repeatCourtCaseJourney.cy.ts
@@ -26,6 +26,7 @@ import OffenceSentenceTypePage from '../pages/offenceSentenceTypePage'
 import Page from '../pages/page'
 import StartPage from '../pages/startPage'
 import CourtCaseOverallConvictionDatePage from '../pages/courtCaseOverallConvictionDatePage'
+import OffenceOffenceOutcomePage from '../pages/offenceOffenceOutcomePage'
 
 context('Repeat Court Case journey', () => {
   const futureDate = dayjs().add(10, 'day')
@@ -266,9 +267,7 @@ context('Repeat Court Case journey', () => {
     Page.verifyOnPageTitle(CourtCaseConfirmationPage, 'Appearance')
   })
 
-  // currently failing due to the changes put in for new sentence journey. will need to be updated once all the new journey is in place.
-  it.skip('remand to sentencing journey', () => {
-    cy.task('stubGetAllAppearanceOutcomes')
+  it('remand to sentencing', () => {
     cy.task('stubGetSentenceTypesByIds', [
       {
         sentenceTypeUuid: '467e2fa8-fce1-41a4-8110-b378c727eed3',
@@ -290,6 +289,11 @@ context('Repeat Court Case journey', () => {
     ])
 
     cy.task('stubGetChargeOutcomesByIds', [
+      {
+        outcomeUuid: '85ffc6bf-6a2c-4f2b-8db8-5b466b602537',
+        outcomeName: 'Imprisonment',
+        outcomeType: 'SENTENCING',
+      },
       {
         outcomeUuid: '63920fee-e43a-45ff-a92d-4679f1af2527',
         outcomeName: 'Imprisonment by default',
@@ -344,17 +348,6 @@ context('Repeat Court Case journey', () => {
     courtCaseSelectCourtNamePage.radioLabelSelector('true').click()
     courtCaseSelectCourtNamePage.continueButton().click()
 
-    const courtCaseOverallCaseOutcomePage = Page.verifyOnPageTitle(
-      CourtCaseOverallCaseOutcomePage,
-      'Select the overall case outcome',
-    )
-    courtCaseOverallCaseOutcomePage.radioLabelContains('Imprisonment').click()
-    courtCaseOverallCaseOutcomePage.continueButton().click()
-
-    const courtCaseCaseOutcomeAppliedAllPage = Page.verifyOnPage(CourtCaseCaseOutcomeAppliedAllPage)
-    courtCaseCaseOutcomeAppliedAllPage.radioLabelSelector('true').click()
-    courtCaseCaseOutcomeAppliedAllPage.continueButton().click()
-
     const courtCaseTaggedBailPage = Page.verifyOnPage(CourtCaseTaggedBailPage)
     courtCaseTaggedBailPage.radioLabelSelector('true').click()
     courtCaseTaggedBailPage.input().type('5')
@@ -366,213 +359,6 @@ context('Repeat Court Case journey', () => {
       'Court case reference': 'C894623',
       'Warrant date': '12/05/2023',
       'Court name': 'Accrington Youth Court',
-      'Overall case outcome': 'Imprisonment',
-      'Does this apply to all offences on the warrant?': 'Yes',
-      'Tagged bail': '5 days',
-    })
-    courtCaseCheckAnswersPage.continueButton().click()
-
-    courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court appearance')
-    courtCaseTaskListPage
-      .taskList()
-      .getTaskList()
-      .should('deep.equal', [
-        {
-          name: 'Add appearance information',
-          status: 'Completed',
-        },
-        {
-          name: 'Update offence outcomes',
-          status: 'Incomplete',
-        },
-        {
-          name: 'Add next court appearance',
-          status: 'Optional',
-        },
-        {
-          name: 'Upload court documents',
-          status: 'Optional',
-        },
-      ])
-
-    courtCaseTaskListPage.updateOffenceOutcomesLink().click()
-
-    const courtCaseOverallSentenceLengthPage = Page.verifyOnPage(CourtCaseOverallSentenceLengthPage)
-    courtCaseOverallSentenceLengthPage.radioLabelSelector('true').click()
-    courtCaseOverallSentenceLengthPage.yearsInput().type('4')
-    courtCaseOverallSentenceLengthPage.monthsInput().type('5')
-    courtCaseOverallSentenceLengthPage.continueButton().click()
-
-    const courtCaseOverallConvictionDatePage = Page.verifyOnPage(CourtCaseOverallConvictionDatePage)
-    courtCaseOverallConvictionDatePage.radioLabelSelector('true').click()
-    courtCaseOverallConvictionDatePage.dayDateInput('overallConvictionDate').clear().type('12')
-    courtCaseOverallConvictionDatePage.monthDateInput('overallConvictionDate').clear().type('5')
-    courtCaseOverallConvictionDatePage.yearDateInput('overallConvictionDate').clear().type('2023')
-    courtCaseOverallConvictionDatePage.continueButton().click()
-
-    let offenceReviewOffencesPage = Page.verifyOnPage(OffenceReviewOffencesPage)
-    offenceReviewOffencesPage.addAnotherButton().click()
-
-    const offenceOffenceDatePage = Page.verifyOnPageTitle(OffenceOffenceDatePage, 'Enter the offence date')
-    offenceOffenceDatePage.dayDateInput('offenceStartDate').type('12')
-    offenceOffenceDatePage.monthDateInput('offenceStartDate').type('5')
-    offenceOffenceDatePage.yearDateInput('offenceStartDate').type('2023')
-    offenceOffenceDatePage.continueButton().click()
-
-    const offenceOffenceCodePage = Page.verifyOnPage(OffenceOffenceCodePage)
-    offenceOffenceCodePage.input().type('PS90037')
-    offenceOffenceCodePage.continueButton().click()
-
-    const offenceOffenceCodeConfirmPage = Page.verifyOnPage(OffenceOffenceCodeConfirmPage)
-    offenceOffenceCodeConfirmPage.continueButton().click()
-
-    const offenceCountNumberPage = Page.verifyOnPage(OffenceCountNumberPage)
-    offenceCountNumberPage.radioLabelSelector('true').click()
-    offenceCountNumberPage.input().type('1')
-    offenceCountNumberPage.continueButton().click()
-
-    const offenceSentenceTypePage = Page.verifyOnPage(OffenceSentenceTypePage)
-    offenceSentenceTypePage.radioLabelContains('SDS (Standard Determinate Sentence)').click()
-    offenceSentenceTypePage.continueButton().click()
-
-    const offencePeriodLengthPage = Page.verifyOnPageTitle(OffencePeriodLengthPage, 'sentence length')
-    offencePeriodLengthPage.yearsInput().type('4')
-    offencePeriodLengthPage.monthsInput().type('5')
-    offencePeriodLengthPage.continueButton().click()
-
-    const cffenceSentenceServeTypePage = Page.verifyOnPage(OffenceSentenceServeTypePage)
-    cffenceSentenceServeTypePage.radioLabelSelector('FORTHWITH').click()
-    cffenceSentenceServeTypePage.continueButton().click()
-
-    offenceReviewOffencesPage = Page.verifyOnPage(OffenceReviewOffencesPage)
-    offenceReviewOffencesPage.radioLabelSelector('true').click()
-    offenceReviewOffencesPage.continueButton().click()
-
-    courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court appearance')
-    courtCaseTaskListPage
-      .taskList()
-      .getTaskList()
-      .should('deep.equal', [
-        {
-          name: 'Add appearance information',
-          status: 'Completed',
-        },
-        {
-          name: 'Update offence outcomes',
-          status: 'Completed',
-        },
-        {
-          name: 'Add next court appearance',
-          status: 'Optional',
-        },
-        {
-          name: 'Upload court documents',
-          status: 'Optional',
-        },
-      ])
-
-    courtCaseTaskListPage.continueButton().click()
-
-    cy.task('verifyCreateSentenceCourtAppearanceRequest').should('equal', 1)
-    Page.verifyOnPageTitle(CourtCaseConfirmationPage, 'Appearance')
-  })
-
-  // currently failing due to the changes put in for new sentence journey. will need to be updated once all the new journey is in place.
-  it.skip('remand to sentencing journey with only one outcome option', () => {
-    cy.task('stubGetAllAppearanceOutcomesWithSingleResults')
-    cy.task('stubGetSentenceTypesByIds', [
-      {
-        sentenceTypeUuid: '467e2fa8-fce1-41a4-8110-b378c727eed3',
-        description: 'SDS (Standard Determinate Sentence)',
-        classification: 'STANDARD',
-      },
-    ])
-    cy.task('stubGetAppearanceOutcomeById', {
-      outcomeUuid: '4b2a225e-5bb1-4bf7-8719-6ff9f3ee0d10',
-      outcomeName: 'Imprisonment',
-      outcomeType: 'SENTENCING',
-    })
-    cy.task('stubGetChargeOutcomesByIds', [
-      {
-        outcomeUuid: '85ffc6bf-6a2c-4f2b-8db8-5b466b602537',
-        outcomeName: 'Imprisonment',
-        outcomeType: 'SENTENCING',
-      },
-    ])
-
-    cy.task('stubGetChargeOutcomesByIds', [
-      {
-        outcomeUuid: '63920fee-e43a-45ff-a92d-4679f1af2527',
-        outcomeName: 'Imprisonment by default',
-        outcomeType: 'SENTENCING',
-      },
-    ])
-    const startPage = Page.verifyOnPage(StartPage)
-    startPage.addAppearanceLink('3fa85f64-5717-4562-b3fc-2c963f66afa6', '2').click()
-
-    const courtCaseWarrantTypePage = Page.verifyOnPage(CourtCaseWarrantTypePage)
-    courtCaseWarrantTypePage.radioLabelSelector('SENTENCING').click()
-    courtCaseWarrantTypePage.continueButton().click()
-
-    let courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court appearance')
-    courtCaseTaskListPage
-      .taskList()
-      .getTaskList()
-      .should('deep.equal', [
-        {
-          name: 'Add appearance information',
-          status: 'Incomplete',
-        },
-        {
-          name: 'Update offence outcomes',
-          status: 'Cannot start yet',
-        },
-        {
-          name: 'Add next court appearance',
-          status: 'Cannot start yet',
-        },
-        {
-          name: 'Upload court documents',
-          status: 'Cannot start yet',
-        },
-      ])
-    courtCaseTaskListPage.appearanceInformationLink().click()
-
-    const courtCaseSelectReferencePage = Page.verifyOnPageTitle(CourtCaseSelectReferencePage, 'C894623')
-    courtCaseSelectReferencePage.radioLabelSelector('true').click()
-    courtCaseSelectReferencePage.continueButton().click()
-
-    const courtCaseWarrantDatePage = Page.verifyOnPage(CourtCaseWarrantDatePage)
-    courtCaseWarrantDatePage.dayDateInput('warrantDate').type('12')
-    courtCaseWarrantDatePage.monthDateInput('warrantDate').type('5')
-    courtCaseWarrantDatePage.yearDateInput('warrantDate').type('2023')
-    courtCaseWarrantDatePage.continueButton().click()
-
-    const courtCaseSelectCourtNamePage = Page.verifyOnPageTitle(
-      CourtCaseSelectCourtNamePage,
-      'Was the appearance at Accrington Youth Court?',
-    )
-    courtCaseSelectCourtNamePage.radioLabelSelector('true').click()
-    courtCaseSelectCourtNamePage.continueButton().click()
-
-    const courtCaseCaseOutcomeAppliedAllPage = Page.verifyOnPage(CourtCaseCaseOutcomeAppliedAllPage)
-    courtCaseCaseOutcomeAppliedAllPage.bodyText().trimTextContent().should('equal', 'Imprisonment')
-    courtCaseCaseOutcomeAppliedAllPage.radioLabelSelector('true').click()
-    courtCaseCaseOutcomeAppliedAllPage.continueButton().click()
-
-    const courtCaseTaggedBailPage = Page.verifyOnPage(CourtCaseTaggedBailPage)
-    courtCaseTaggedBailPage.radioLabelSelector('true').click()
-    courtCaseTaggedBailPage.input().type('5')
-    courtCaseTaggedBailPage.continueButton().click()
-
-    const courtCaseCheckAnswersPage = Page.verifyOnPage(CourtCaseCheckAnswersPage)
-    courtCaseCheckAnswersPage.summaryList().getSummaryList().should('deep.equal', {
-      'Warrant type': 'Sentencing',
-      'Court case reference': 'C894623',
-      'Warrant date': '12/05/2023',
-      'Court name': 'Accrington Youth Court',
-      'Overall case outcome': 'Imprisonment',
-      'Does this apply to all offences on the warrant?': 'Yes',
       'Tagged bail': '5 days',
     })
     courtCaseCheckAnswersPage.continueButton().click()
@@ -630,6 +416,13 @@ context('Repeat Court Case journey', () => {
     const offenceOffenceCodeConfirmPage = Page.verifyOnPage(OffenceOffenceCodeConfirmPage)
     offenceOffenceCodeConfirmPage.continueButton().click()
 
+    const offenceOffenceOutcomePage = Page.verifyOnPageTitle(
+      OffenceOffenceOutcomePage,
+      'Select the outcome for this offence',
+    )
+    offenceOffenceOutcomePage.radioLabelContains('Imprisonment').click()
+    offenceOffenceOutcomePage.continueButton().click()
+
     const offenceCountNumberPage = Page.verifyOnPage(OffenceCountNumberPage)
     offenceCountNumberPage.radioLabelSelector('true').click()
     offenceCountNumberPage.input().type('1')
@@ -679,36 +472,5 @@ context('Repeat Court Case journey', () => {
 
     cy.task('verifyCreateSentenceCourtAppearanceRequest').should('equal', 1)
     Page.verifyOnPageTitle(CourtCaseConfirmationPage, 'Appearance')
-  })
-
-  it('Add new sentencing-appearance where the court is the same as the previous one navigates to tagged bail)', () => {
-    const startPage = Page.verifyOnPage(StartPage)
-    startPage.addAppearanceLink('3fa85f64-5717-4562-b3fc-2c963f66afa6', '2').click()
-
-    const courtCaseWarrantTypePage = Page.verifyOnPage(CourtCaseWarrantTypePage)
-    courtCaseWarrantTypePage.radioLabelSelector('SENTENCING').click()
-    courtCaseWarrantTypePage.continueButton().click()
-
-    const courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court appearance')
-    courtCaseTaskListPage.appearanceInformationLink().click()
-
-    const courtCaseSelectReferencePage = Page.verifyOnPageTitle(CourtCaseSelectReferencePage, 'C894623')
-    courtCaseSelectReferencePage.radioLabelSelector('true').click()
-    courtCaseSelectReferencePage.continueButton().click()
-
-    const courtCaseWarrantDatePage = Page.verifyOnPage(CourtCaseWarrantDatePage)
-    courtCaseWarrantDatePage.dayDateInput('warrantDate').type('12')
-    courtCaseWarrantDatePage.monthDateInput('warrantDate').type('5')
-    courtCaseWarrantDatePage.yearDateInput('warrantDate').type('2023')
-    courtCaseWarrantDatePage.continueButton().click()
-
-    const courtCaseSelectCourtNamePage = Page.verifyOnPageTitle(
-      CourtCaseSelectCourtNamePage,
-      'Was the appearance at Accrington Youth Court?',
-    )
-    courtCaseSelectCourtNamePage.radioLabelSelector('true').click()
-    courtCaseSelectCourtNamePage.continueButton().click()
-
-    Page.verifyOnPage(CourtCaseTaggedBailPage)
   })
 })

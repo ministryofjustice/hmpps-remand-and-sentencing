756ba77d1416eb2c262722849d318d83b0d27890
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2025-04-29 17:00:34 +0100

    RASS-1005 add hint inset to overall conviction date

--- integration_tests/pages/courtCaseOverallConvictionDatePage.ts
+++ integration_tests/pages/courtCaseOverallConvictionDatePage.ts
@@ -1,7 +1,9 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class CourtCaseOverallConvictionDatePage extends Page {
   constructor() {
     super('Is the conviction date the same for all offences on the warrant?')
   }
+
+  hintInset = (): PageElement => cy.get('[data-qa=hintInset]')
 }

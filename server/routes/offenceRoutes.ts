4c20c6a88748ca7534c2d1bb83f4c46ddb84c574
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-05-19 14:48:51 +0100

    RASS-1080 set query parameter correctly

--- server/routes/offenceRoutes.ts
+++ server/routes/offenceRoutes.ts
@@ -1236,7 +1236,7 @@ export default class OffenceRoutes extends BaseRoutes {
       req.flash('errors', errors)
       req.flash('offenceAlternativeSentenceLengthForm', { ...offenceAlternativeSentenceLengthForm })
       return res.redirect(
-        `/person/${nomsId}/${addOrEditCourtCase}/${courtCaseReference}/${addOrEditCourtAppearance}/${appearanceReference}/offences/${offenceReference}/alternative-period-length?periodLengthType=${periodLengthType}&${submitToEditOffence ? 'submitToEditOffence=true' : ''}`,
+        `/person/${nomsId}/${addOrEditCourtCase}/${courtCaseReference}/${addOrEditCourtAppearance}/${appearanceReference}/offences/${offenceReference}/alternative-period-length?periodLengthType=${periodLengthType}${submitToEditOffence ? '&submitToEditOffence=true' : ''}`,
       )
     }
     if (submitToEditOffence) {

ec1683fa69ed427d1bac7cd0dde8d259e0f45b2e
Yasin Mustafa <yasin.mustafa@gmail.com>
2025-07-01 17:51:19 +0100

    code tidy

--- integration_tests/e2e/sentencingMakingSentenceConcurrent.cy.ts
+++ integration_tests/e2e/sentencingMakingSentenceConcurrent.cy.ts
@@ -5,6 +5,17 @@ import OffenceSentenceServeTypePage from '../pages/offenceSentenceServeTypePage'
 import Page from '../pages/page'
 import SentencingMakingSentenceConcurrentPage from '../pages/sentencingMakingSentenceConcurrentPage'
 
+function navigateMakeSentencingConcurrent() {
+  const offenceCheckOffenceAnswersPage = new OffenceCheckOffenceAnswersPage('You have added 2 offence')
+  offenceCheckOffenceAnswersPage.editOffenceLink('A1234AB', '0', '0', '0').click()
+  const offenceEditOffencePage = Page.verifyOnPageTitle(OffenceEditOffencePage, 'offence')
+  offenceEditOffencePage.editFieldLink('A1234AB', 'add', '0', 'add', '0', '0', 'sentence-serve-type').click()
+  const offenceSentenceServeTypePage = Page.verifyOnPage(OffenceSentenceServeTypePage)
+  offenceSentenceServeTypePage.radioLabelSelector('CONCURRENT').click()
+  offenceSentenceServeTypePage.continueButton().click()
+  return Page.verifyOnPage(SentencingMakingSentenceConcurrentPage)
+}
+
 context('Sentencing making sentence concurrent Page', () => {
   let sentencingMakingSentenceConcurrentPage: SentencingMakingSentenceConcurrentPage
   beforeEach(() => {
@@ -43,27 +54,32 @@ context('Sentencing making sentence concurrent Page', () => {
     courtCaseWarrantDatePage.continueButton().click()
     cy.createSentencedOffence('A1234AB', '0', '0', '0')
     cy.createSentencedOffenceConsecutiveTo('A1234AB', '0', '0', '1')
-    cy.createSentencedOffenceConsecutiveTo('A1234AB', '0', '0', '2', '3', '1|SAME')
-    // const offenceCheckOffenceAnswersPage = new OffenceCheckOffenceAnswersPage('You have added 3 offences')
-    // offenceCheckOffenceAnswersPage.editOffenceLink('A1234AB', '0', '0', '0').click()
-    // const offenceEditOffencePage = Page.verifyOnPageTitle(OffenceEditOffencePage, 'offence')
-    // offenceEditOffencePage.editFieldLink('A1234AB', 'add', '0', 'add', '0', '0', 'sentence-serve-type').click()
-    // const offenceSentenceServeTypePage = Page.verifyOnPage(OffenceSentenceServeTypePage)
-    // offenceSentenceServeTypePage.radioLabelSelector('CONCURRENT').click()
-    // offenceSentenceServeTypePage.continueButton().click()
-    // sentencingMakingSentenceConcurrentPage = Page.verifyOnPage(SentencingMakingSentenceConcurrentPage)
   })
 
-  // it('displays person details', () => {
-  //   sentencingMakingSentenceConcurrentPage
-  //     .prisonerBanner()
-  //     .should('contain.text', 'Meza, Cormac')
-  //     .and('contain.text', 'A1234AB')
-  //     .and('contain.text', 'EstablishmentHMP Bedford')
-  //     .and('contain.text', 'Cell numberCELL-1')
-  // })
-  //
+  it('displays person details', () => {
+    const offenceCheckOffenceAnswersPage = new OffenceCheckOffenceAnswersPage('You have added 2 offence')
+    offenceCheckOffenceAnswersPage.editOffenceLink('A1234AB', '0', '0', '0').click()
+    const offenceEditOffencePage = Page.verifyOnPageTitle(OffenceEditOffencePage, 'offence')
+    offenceEditOffencePage.editFieldLink('A1234AB', 'add', '0', 'add', '0', '0', 'sentence-serve-type').click()
+    const offenceSentenceServeTypePage = Page.verifyOnPage(OffenceSentenceServeTypePage)
+    offenceSentenceServeTypePage.radioLabelSelector('CONCURRENT').click()
+    offenceSentenceServeTypePage.continueButton().click()
+    sentencingMakingSentenceConcurrentPage = Page.verifyOnPage(SentencingMakingSentenceConcurrentPage)
+    sentencingMakingSentenceConcurrentPage
+      .prisonerBanner()
+      .should('contain.text', 'Meza, Cormac')
+      .and('contain.text', 'A1234AB')
+      .and('contain.text', 'EstablishmentHMP Bedford')
+      .and('contain.text', 'Cell numberCELL-1')
+  })
+
   it('button to continue is displayed', () => {
+    sentencingMakingSentenceConcurrentPage = navigateMakeSentencingConcurrent()
+    sentencingMakingSentenceConcurrentPage.continueButton().should('contain.text', 'Yes, make the sentence concurrent')
+  })
+
+  it('button to continue is displayed 2', () => {
+    cy.createSentencedOffenceConsecutiveTo('A1234AB', '0', '0', '2', '3', '1|SAME')
     const offenceCheckOffenceAnswersPage = new OffenceCheckOffenceAnswersPage('You have added 3 offences')
     offenceCheckOffenceAnswersPage.editOffenceLink('A1234AB', '0', '0', '2').click()
     const offenceEditOffencePage = Page.verifyOnPageTitle(OffenceEditOffencePage, 'offence')

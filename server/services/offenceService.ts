8438f3fecd363efc1b49cef00dbfb3a7cb49704b
Yasin Mustafa <yasin.mustafa@gmail.com>
2025-07-10 12:44:28 +0100

    remove console.logs

--- server/services/offenceService.ts
+++ server/services/offenceService.ts
@@ -681,8 +681,6 @@ export default class OffenceService {
 
         const offenceStartDate = dayjs(offence.offenceStartDate)
         const offenceEndDate = offence.offenceEndDate ? dayjs(offence.offenceEndDate) : null
-        console.log(`Offence start ${offenceStartDate}`)
-        console.log(`Offence end ${offenceEndDate}`)
 
         if (offenceEndDate && !convictionDate.isAfter(offenceEndDate)) {
           return [

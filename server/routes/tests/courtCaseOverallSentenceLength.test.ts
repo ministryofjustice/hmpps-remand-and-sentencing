c21ece29e0a7aaafaceec30b5face00d1ca7e0ac
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-07-25 15:19:39 +0100

    more converts

---
+++ server/routes/tests/courtCaseOverallSentenceLength.test.ts
@@ -0,0 +1,37 @@
+import type { Express } from 'express'
+import * as cheerio from 'cheerio'
+import request from 'supertest'
+import { appWithAllRoutes, defaultServices } from '../testutils/appSetup'
+
+let app: Express
+
+beforeEach(() => {
+  app = appWithAllRoutes({})
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('GET Court Case Sentence Length', () => {
+  it('should render page on new journey', () => {
+    defaultServices.courtAppearanceService.getSessionCourtAppearance.mockReturnValue({
+      warrantType: 'SENTENCING',
+    })
+    return request(app)
+      .get('/person/A1234AB/add-court-case/0/add-court-appearance/0/sentencing/overall-sentence-length')
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        const $ = cheerio.load(res.text)
+        const prisonerBanner = $('.mini-profile').text()
+        expect(prisonerBanner).toContain('Meza, Cormac')
+        expect(prisonerBanner).toContain('A1234AB')
+        expect(prisonerBanner).toContain('EstablishmentHMP Bedford')
+        expect(prisonerBanner).toContain('Cell numberCELL-1')
+        const continueButton = $('[data-qa=continue-button]').text()
+        expect(continueButton).toContain('Continue')
+        const caption = $('[data-qa=warrant-information-caption]').text()
+        expect(caption).toContain('Add overall warrant information')
+      })
+  })
+})

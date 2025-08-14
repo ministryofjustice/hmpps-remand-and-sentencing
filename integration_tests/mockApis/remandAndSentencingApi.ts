d811f26c8dedca635dc71e2bf3c4a69619993e4d
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-08-14 10:08:27 +0100

    revert

--- integration_tests/mockApis/remandAndSentencingApi.ts
+++ integration_tests/mockApis/remandAndSentencingApi.ts
@@ -899,7 +899,7 @@ export default {
     return stubFor({
       request: {
         method: 'PUT',
-        urlPathPattern:
+        urlPattern:
           '/remand-and-sentencing-api/court-appearance/([a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})',
         bodyPatterns: [
           {

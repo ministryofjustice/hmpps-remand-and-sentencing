53cc94c50e87f5c90cd64a8e398c6ed3bdc2183d
Yasin Mustafa <yasin.mustafa@gmail.com>
2025-09-10 20:51:18 +0100

    test fix

--- server/services/remandAndSentencingService.ts
+++ server/services/remandAndSentencingService.ts
@@ -225,6 +225,7 @@ export default class RemandAndSentencingService {
     sentenceUuids: string[],
     username: string,
   ): Promise<SentenceConsecutiveToDetailsResponse> {
+    console.log(`ALL >>> = ${JSON.stringify(sentenceUuids.filter(sentenceUuid => sentenceUuid))}`)
     console.log(`length >>> = ${sentenceUuids.filter(sentenceUuid => sentenceUuid).length}`)
     return sentenceUuids.filter(sentenceUuid => sentenceUuid).length
       ? this.remandAndSentencingApiClient.consecutiveToDetails(sentenceUuids, username)

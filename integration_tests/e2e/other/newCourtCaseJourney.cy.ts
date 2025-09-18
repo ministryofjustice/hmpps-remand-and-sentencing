44eeba5e8c704c31055a3a11ce818eb595713bbc
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2025-09-18 10:00:03 +0100

    RASS-1307-fixing test

--- integration_tests/e2e/other/newCourtCaseJourney.cy.ts
+++ integration_tests/e2e/other/newCourtCaseJourney.cy.ts
@@ -129,7 +129,7 @@ context('New Court Case journey', () => {
       'Warrant date': '13/05/2023',
       'Court name': 'Accrington Youth Court',
       'Overall case outcome': 'Remanded in custody',
-      'Is the outcome the same for all offences on the warrant?': 'No',
+      'Does this outcome apply to all offences on the warrant?': 'No',
     })
     courtCaseCheckAnswersPage.continueButton().click()
 

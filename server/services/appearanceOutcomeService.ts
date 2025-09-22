ebad6a65103651c08902c76e0e4947e5d014ad48
Yasin Mustafa <yasin.mustafa@gmail.com>
2025-09-22 15:56:59 +0100

    Call the api to get outcome-by-uuid if the cached outcomes dont have it

--- server/services/appearanceOutcomeService.ts
+++ server/services/appearanceOutcomeService.ts
@@ -11,7 +11,12 @@ export default class AppearanceOutcomeService {
   }
 
   async getOutcomeByUuid(appearanceOutcomeUuid: string, username: string): Promise<AppearanceOutcome> {
-    const outcomes = await this.getAllOutcomes(username)
-    return outcomes.find(o => o.outcomeUuid === appearanceOutcomeUuid)
+    const allActiveOutcomes = await this.getAllOutcomes(username)
+    const outcome = allActiveOutcomes.find(o => o.outcomeUuid === appearanceOutcomeUuid)
+    if (outcome) {
+      return outcome
+    }
+    // There is the potential that the outcome for the appearanceOutcomeUuid is inactive, therefore call the api in this scenario
+    return this.remandAndSentencingApiClient.getAppearanceOutcomeByUuid(appearanceOutcomeUuid, username)
   }
 }

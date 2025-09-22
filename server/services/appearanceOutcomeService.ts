cc73d2712b99183149587c2447774a3540c1c47b
Yasin Mustafa <yasin.mustafa@gmail.com>
2025-09-22 11:43:43 +0100

    RASS-874 Add ref data cache in redis and use it for getAllOutcomes call (#1041)
    * RASS-874 Add ref data cache in redis and use it for getAllOutcomes call
    
    * Add some logging and tidy up
    
    * Only use redis cache for one ref data call for the time being (for testing)

--- server/services/appearanceOutcomeService.ts
+++ server/services/appearanceOutcomeService.ts
@@ -1,11 +1,13 @@
-import type { AppearanceOutcome } from '../@types/remandAndSentencingApi/remandAndSentencingClientTypes'
+import getOrSetRefDataInCache from '../cache/refDataCache'
 import RemandAndSentencingApiClient from '../data/remandAndSentencingApiClient'
+import { AppearanceOutcome } from '../@types/remandAndSentencingApi/remandAndSentencingClientTypes'
 
 export default class AppearanceOutcomeService {
   constructor(private readonly remandAndSentencingApiClient: RemandAndSentencingApiClient) {}
 
   async getAllOutcomes(username: string): Promise<AppearanceOutcome[]> {
-    return this.remandAndSentencingApiClient.getAllAppearanceOutcomes(username)
+    const cacheKey = 'appearance-outcomes:all'
+    return getOrSetRefDataInCache(cacheKey, () => this.remandAndSentencingApiClient.getAllAppearanceOutcomes(username))
   }
 
   async getOutcomeByUuid(appearanceOutcomeUuid: string, username: string): Promise<AppearanceOutcome> {

dc0d0955f6de6faf481e463daa131cf1251e46ca
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-09-24 11:54:01 +0100

    RASS-1354 styling changes on consecutive to pages

--- integration_tests/pages/sentenceSentenceConsecutiveToPage.ts
+++ integration_tests/pages/sentenceSentenceConsecutiveToPage.ts
@@ -2,7 +2,7 @@ import Page, { PageElement } from './page'
 
 export default class SentenceConsecutiveToPage extends Page {
   constructor() {
-    super('Which sentence is this sentence consecutive to?')
+    super('Select the sentence this is consecutive to')
   }
 
   sentencesOnOtherCasesHeader = (): PageElement => cy.get('[data-qa=sentencesOnOtherCasesHeader]')

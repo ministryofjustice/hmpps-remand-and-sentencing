f11abaf4e138ff2139573e3713a3d8fb989afb02
Yasin Mustafa <yasin.mustafa@gmail.com>
2025-09-25 16:43:23 +0100

    Lint fix (revert tep test change)

--- integration_tests/e2e/other/newCourtCaseJourney.cy.ts
+++ integration_tests/e2e/other/newCourtCaseJourney.cy.ts
@@ -51,225 +51,225 @@ context('New Court Case journey', () => {
   })
 
   it('fill in remand journey', () => {
-    // cy.task('stubGetChargeOutcomesByIds', [
-    //   {
-    //     outcomeUuid: '85ffc6bf-6a2c-4f2b-8db8-5b466b602537',
-    //     outcomeName: 'Remanded in custody',
-    //     outcomeType: 'REMAND',
-    //   },
-    // ])
-    // cy.task('stubGetChargeOutcomeById', {
-    //   outcomeUuid: '85ffc6bf-6a2c-4f2b-8db8-5b466b602537',
-    //   outcomeName: 'Remanded in custody',
-    //   outcomeType: 'REMAND',
-    // })
-    // cy.task('stubGetAppearanceTypeByUuid')
-    // const startPage = Page.verifyOnPage(StartPage)
-    // startPage.actionListLink().click()
-    //
-    // const courtCaseWarrantTypePage = Page.verifyOnPage(CourtCaseWarrantTypePage)
-    // courtCaseWarrantTypePage.radioLabelSelector('REMAND').click()
-    // courtCaseWarrantTypePage.continueButton().click()
-    //
-    // let courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court case')
-    // courtCaseTaskListPage
-    //   .taskList()
-    //   .getTaskList()
-    //   .should('deep.equal', [
-    //     {
-    //       name: 'Add appearance information',
-    //       status: 'Incomplete',
-    //     },
-    //     {
-    //       name: 'Add offences',
-    //       status: 'Cannot start yet',
-    //     },
-    //     {
-    //       name: 'Next court appearance',
-    //       status: 'Cannot start yet',
-    //     },
-    //     {
-    //       name: 'Upload court documents',
-    //       status: 'Cannot start yet',
-    //     },
-    //   ])
-    // courtCaseTaskListPage.appearanceInformationLink().click()
-    //
-    // const courtCaseReferencePage = Page.verifyOnPageTitle(CourtCaseReferencePage, 'Enter the case reference')
-    // courtCaseReferencePage.input().type('T12345678')
-    // courtCaseReferencePage.continueButton().click()
-    // const courtCaseWarrantDatePage = Page.verifyOnPage(CourtCaseWarrantDatePage)
-    // courtCaseWarrantDatePage.dayDateInput('warrantDate').type('13')
-    // courtCaseWarrantDatePage.monthDateInput('warrantDate').type('5')
-    // courtCaseWarrantDatePage.yearDateInput('warrantDate').type('2023')
-    // courtCaseWarrantDatePage.continueButton().click()
-    // const courtCaseCourtNamePage = Page.verifyOnPageTitle(CourtCaseCourtNamePage, 'What is the court name?')
-    // courtCaseCourtNamePage.autoCompleteInput().type('cou')
-    // courtCaseCourtNamePage.firstAutoCompleteOption().contains('Accrington Youth Court')
-    // courtCaseCourtNamePage.firstAutoCompleteOption().click()
-    // courtCaseCourtNamePage.continueButton().click()
-    //
-    // const courtCaseOverallCaseOutcomePage = Page.verifyOnPageTitle(
-    //   CourtCaseOverallCaseOutcomePage,
-    //   'Select the overall case outcome',
-    // )
-    // courtCaseOverallCaseOutcomePage.radioLabelContains('Remanded in custody').click()
-    // courtCaseOverallCaseOutcomePage.continueButton().click()
-    //
-    // const courtCaseCaseOutcomeAppliedAllPage = Page.verifyOnPage(CourtCaseCaseOutcomeAppliedAllPage)
-    // courtCaseCaseOutcomeAppliedAllPage.bodyText().should('contain.text', 'Remanded in custody')
-    //
-    // courtCaseCaseOutcomeAppliedAllPage.radioLabelSelector('false').click()
-    // courtCaseCaseOutcomeAppliedAllPage.continueButton().click()
-    //
-    // const courtCaseCheckAnswersPage = Page.verifyOnPage(CourtCaseCheckAnswersPage)
-    // courtCaseCheckAnswersPage.summaryList().getSummaryList().should('deep.equal', {
-    //   'Warrant type': 'Remand',
-    //   'Case reference': 'T12345678',
-    //   'Warrant date': '13/05/2023',
-    //   'Court name': 'Accrington Youth Court',
-    //   'Overall case outcome': 'Remanded in custody',
-    //   'Does this outcome apply to all offences on the warrant?': 'No',
-    // })
-    // courtCaseCheckAnswersPage.continueButton().click()
-    //
-    // courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court case')
-    // courtCaseTaskListPage
-    //   .taskList()
-    //   .getTaskList()
-    //   .should('deep.equal', [
-    //     {
-    //       name: 'Add appearance information',
-    //       status: 'Completed',
-    //     },
-    //     {
-    //       name: 'Add offences',
-    //       status: 'Incomplete',
-    //     },
-    //     {
-    //       name: 'Next court appearance',
-    //       status: 'Incomplete',
-    //     },
-    //     {
-    //       name: 'Upload court documents',
-    //       status: 'Optional',
-    //     },
-    //   ])
-    // courtCaseTaskListPage.offencesLink().click()
-    //
-    // const offenceOffenceDatePage = Page.verifyOnPageTitle(
-    //   OffenceOffenceDatePage,
-    //   'Enter the offence dates for the first offence',
-    // )
-    // offenceOffenceDatePage.dayDateInput('offenceStartDate').type('10')
-    // offenceOffenceDatePage.monthDateInput('offenceStartDate').type('5')
-    // offenceOffenceDatePage.yearDateInput('offenceStartDate').type('2023')
-    // offenceOffenceDatePage.continueButton().click()
-    //
-    // const offenceOffenceCodePage = Page.verifyOnPage(OffenceOffenceCodePage)
-    // offenceOffenceCodePage.appearanceDetailsSummaryList().getSummaryList().should('deep.equal', {
-    //   'Case reference': 'T12345678',
-    //   'Court name': 'Accrington Youth Court',
-    //   'Warrant date': '13/05/2023',
-    //   'Overall case outcome': 'Remanded in custody',
-    // })
-    // offenceOffenceCodePage.input().type('PS90037')
-    // offenceOffenceCodePage.continueButton().click()
-    //
-    // const offenceOffenceCodeConfirmPage = Page.verifyOnPage(OffenceOffenceCodeConfirmPage)
-    // offenceOffenceCodeConfirmPage.continueButton().click()
-    //
-    // const offenceOutcomePage = Page.verifyOnPageTitle(OffenceOffenceOutcomePage, 'Select the outcome for this offence')
-    // offenceOutcomePage.radioLabelContains('Remanded in custody').click()
-    // offenceOutcomePage.continueButton().click()
-    //
-    // const offenceCheckOffenceAnswersPage = new OffenceCheckOffenceAnswersPage('You have added 1 offence')
-    // offenceCheckOffenceAnswersPage.finishedAddingRadio().click()
-    // offenceCheckOffenceAnswersPage.finishAddingButton().click()
-    //
-    // courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court case')
-    // courtCaseTaskListPage
-    //   .taskList()
-    //   .getTaskList()
-    //   .should('deep.equal', [
-    //     {
-    //       name: 'Add appearance information',
-    //       status: 'Completed',
-    //     },
-    //     {
-    //       name: 'Add offences',
-    //       status: 'Completed',
-    //     },
-    //     {
-    //       name: 'Next court appearance',
-    //       status: 'Incomplete',
-    //     },
-    //     {
-    //       name: 'Upload court documents',
-    //       status: 'Optional',
-    //     },
-    //   ])
-    // courtCaseTaskListPage.nextCourtAppearanceLink().click()
-    //
-    // const courtCaseNextHearingSetPage = Page.verifyOnPage(CourtCaseNextHearingSetPage)
-    // courtCaseNextHearingSetPage.radioLabelSelector('true').click()
-    // courtCaseNextHearingSetPage.continueButton().click()
-    //
-    // const courtCaseNextHearingTypePage = Page.verifyOnPage(CourtCaseNextHearingTypePage)
-    // courtCaseNextHearingTypePage.radioLabelContains('Court appearance').click()
-    // courtCaseNextHearingTypePage.continueButton().click()
-    //
-    // const courtCaseNextHearingDatePage = Page.verifyOnPage(CourtCaseNextHearingDatePage)
-    //
-    // courtCaseNextHearingDatePage.dayDateInput('nextHearingDate').type(futureDate.date().toString())
-    // courtCaseNextHearingDatePage.monthDateInput('nextHearingDate').type((futureDate.month() + 1).toString())
-    // courtCaseNextHearingDatePage.yearDateInput('nextHearingDate').type(futureDate.year().toString())
-    // courtCaseNextHearingDatePage.continueButton().click()
-    //
-    // const courtCaseNextHearingCourtSetPage = Page.verifyOnPage(CourtCaseNextHearingCourtSetPage)
-    // courtCaseNextHearingCourtSetPage.radioLabelSelector('true').click()
-    // courtCaseNextHearingCourtSetPage.continueButton().click()
-    //
-    // const courtCaseNextHearingAnswersPage = Page.verifyOnPage(CourtCaseCheckNextHearingAnswersPage)
-    // courtCaseNextHearingAnswersPage
-    //   .summaryList()
-    //   .getSummaryList()
-    //   .should('deep.equal', {
-    //     'Next hearing date': futureDate.format('DD/MM/YYYY'),
-    //     'Next hearing location': 'Accrington Youth Court',
-    //     'Next hearing type': 'Court appearance',
-    //   })
-    // courtCaseNextHearingAnswersPage.continueButton().click()
-    //
-    // courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court case')
-    // courtCaseTaskListPage
-    //   .taskList()
-    //   .getTaskList()
-    //   .should('deep.equal', [
-    //     {
-    //       name: 'Add appearance information',
-    //       status: 'Completed',
-    //     },
-    //     {
-    //       name: 'Add offences',
-    //       status: 'Completed',
-    //     },
-    //     {
-    //       name: 'Next court appearance',
-    //       status: 'Completed',
-    //     },
-    //     {
-    //       name: 'Upload court documents',
-    //       status: 'Optional',
-    //     },
-    //   ])
-    // courtCaseTaskListPage.continueButton().click()
-    //
-    // cy.task('verifyCreateCourtCaseRequest', { nextHearingDate: futureDate.format('YYYY-MM-DD') }).should('equal', 1)
-    // Page.verifyOnPageTitle(CourtCaseConfirmationPage, 'Court case')
+    cy.task('stubGetChargeOutcomesByIds', [
+      {
+        outcomeUuid: '85ffc6bf-6a2c-4f2b-8db8-5b466b602537',
+        outcomeName: 'Remanded in custody',
+        outcomeType: 'REMAND',
+      },
+    ])
+    cy.task('stubGetChargeOutcomeById', {
+      outcomeUuid: '85ffc6bf-6a2c-4f2b-8db8-5b466b602537',
+      outcomeName: 'Remanded in custody',
+      outcomeType: 'REMAND',
+    })
+    cy.task('stubGetAppearanceTypeByUuid')
+    const startPage = Page.verifyOnPage(StartPage)
+    startPage.actionListLink().click()
+
+    const courtCaseWarrantTypePage = Page.verifyOnPage(CourtCaseWarrantTypePage)
+    courtCaseWarrantTypePage.radioLabelSelector('REMAND').click()
+    courtCaseWarrantTypePage.continueButton().click()
+
+    let courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court case')
+    courtCaseTaskListPage
+      .taskList()
+      .getTaskList()
+      .should('deep.equal', [
+        {
+          name: 'Add appearance information',
+          status: 'Incomplete',
+        },
+        {
+          name: 'Add offences',
+          status: 'Cannot start yet',
+        },
+        {
+          name: 'Next court appearance',
+          status: 'Cannot start yet',
+        },
+        {
+          name: 'Upload court documents',
+          status: 'Cannot start yet',
+        },
+      ])
+    courtCaseTaskListPage.appearanceInformationLink().click()
+
+    const courtCaseReferencePage = Page.verifyOnPageTitle(CourtCaseReferencePage, 'Enter the case reference')
+    courtCaseReferencePage.input().type('T12345678')
+    courtCaseReferencePage.continueButton().click()
+    const courtCaseWarrantDatePage = Page.verifyOnPage(CourtCaseWarrantDatePage)
+    courtCaseWarrantDatePage.dayDateInput('warrantDate').type('13')
+    courtCaseWarrantDatePage.monthDateInput('warrantDate').type('5')
+    courtCaseWarrantDatePage.yearDateInput('warrantDate').type('2023')
+    courtCaseWarrantDatePage.continueButton().click()
+    const courtCaseCourtNamePage = Page.verifyOnPageTitle(CourtCaseCourtNamePage, 'What is the court name?')
+    courtCaseCourtNamePage.autoCompleteInput().type('cou')
+    courtCaseCourtNamePage.firstAutoCompleteOption().contains('Accrington Youth Court')
+    courtCaseCourtNamePage.firstAutoCompleteOption().click()
+    courtCaseCourtNamePage.continueButton().click()
+
+    const courtCaseOverallCaseOutcomePage = Page.verifyOnPageTitle(
+      CourtCaseOverallCaseOutcomePage,
+      'Select the overall case outcome',
+    )
+    courtCaseOverallCaseOutcomePage.radioLabelContains('Remanded in custody').click()
+    courtCaseOverallCaseOutcomePage.continueButton().click()
+
+    const courtCaseCaseOutcomeAppliedAllPage = Page.verifyOnPage(CourtCaseCaseOutcomeAppliedAllPage)
+    courtCaseCaseOutcomeAppliedAllPage.bodyText().should('contain.text', 'Remanded in custody')
+
+    courtCaseCaseOutcomeAppliedAllPage.radioLabelSelector('false').click()
+    courtCaseCaseOutcomeAppliedAllPage.continueButton().click()
+
+    const courtCaseCheckAnswersPage = Page.verifyOnPage(CourtCaseCheckAnswersPage)
+    courtCaseCheckAnswersPage.summaryList().getSummaryList().should('deep.equal', {
+      'Warrant type': 'Remand',
+      'Case reference': 'T12345678',
+      'Warrant date': '13/05/2023',
+      'Court name': 'Accrington Youth Court',
+      'Overall case outcome': 'Remanded in custody',
+      'Does this outcome apply to all offences on the warrant?': 'No',
+    })
+    courtCaseCheckAnswersPage.continueButton().click()
+
+    courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court case')
+    courtCaseTaskListPage
+      .taskList()
+      .getTaskList()
+      .should('deep.equal', [
+        {
+          name: 'Add appearance information',
+          status: 'Completed',
+        },
+        {
+          name: 'Add offences',
+          status: 'Incomplete',
+        },
+        {
+          name: 'Next court appearance',
+          status: 'Incomplete',
+        },
+        {
+          name: 'Upload court documents',
+          status: 'Optional',
+        },
+      ])
+    courtCaseTaskListPage.offencesLink().click()
+
+    const offenceOffenceDatePage = Page.verifyOnPageTitle(
+      OffenceOffenceDatePage,
+      'Enter the offence dates for the first offence',
+    )
+    offenceOffenceDatePage.dayDateInput('offenceStartDate').type('10')
+    offenceOffenceDatePage.monthDateInput('offenceStartDate').type('5')
+    offenceOffenceDatePage.yearDateInput('offenceStartDate').type('2023')
+    offenceOffenceDatePage.continueButton().click()
+
+    const offenceOffenceCodePage = Page.verifyOnPage(OffenceOffenceCodePage)
+    offenceOffenceCodePage.appearanceDetailsSummaryList().getSummaryList().should('deep.equal', {
+      'Case reference': 'T12345678',
+      'Court name': 'Accrington Youth Court',
+      'Warrant date': '13/05/2023',
+      'Overall case outcome': 'Remanded in custody',
+    })
+    offenceOffenceCodePage.input().type('PS90037')
+    offenceOffenceCodePage.continueButton().click()
+
+    const offenceOffenceCodeConfirmPage = Page.verifyOnPage(OffenceOffenceCodeConfirmPage)
+    offenceOffenceCodeConfirmPage.continueButton().click()
+
+    const offenceOutcomePage = Page.verifyOnPageTitle(OffenceOffenceOutcomePage, 'Select the outcome for this offence')
+    offenceOutcomePage.radioLabelContains('Remanded in custody').click()
+    offenceOutcomePage.continueButton().click()
+
+    const offenceCheckOffenceAnswersPage = new OffenceCheckOffenceAnswersPage('You have added 1 offence')
+    offenceCheckOffenceAnswersPage.finishedAddingRadio().click()
+    offenceCheckOffenceAnswersPage.finishAddingButton().click()
+
+    courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court case')
+    courtCaseTaskListPage
+      .taskList()
+      .getTaskList()
+      .should('deep.equal', [
+        {
+          name: 'Add appearance information',
+          status: 'Completed',
+        },
+        {
+          name: 'Add offences',
+          status: 'Completed',
+        },
+        {
+          name: 'Next court appearance',
+          status: 'Incomplete',
+        },
+        {
+          name: 'Upload court documents',
+          status: 'Optional',
+        },
+      ])
+    courtCaseTaskListPage.nextCourtAppearanceLink().click()
+
+    const courtCaseNextHearingSetPage = Page.verifyOnPage(CourtCaseNextHearingSetPage)
+    courtCaseNextHearingSetPage.radioLabelSelector('true').click()
+    courtCaseNextHearingSetPage.continueButton().click()
+
+    const courtCaseNextHearingTypePage = Page.verifyOnPage(CourtCaseNextHearingTypePage)
+    courtCaseNextHearingTypePage.radioLabelContains('Court appearance').click()
+    courtCaseNextHearingTypePage.continueButton().click()
+
+    const courtCaseNextHearingDatePage = Page.verifyOnPage(CourtCaseNextHearingDatePage)
+
+    courtCaseNextHearingDatePage.dayDateInput('nextHearingDate').type(futureDate.date().toString())
+    courtCaseNextHearingDatePage.monthDateInput('nextHearingDate').type((futureDate.month() + 1).toString())
+    courtCaseNextHearingDatePage.yearDateInput('nextHearingDate').type(futureDate.year().toString())
+    courtCaseNextHearingDatePage.continueButton().click()
+
+    const courtCaseNextHearingCourtSetPage = Page.verifyOnPage(CourtCaseNextHearingCourtSetPage)
+    courtCaseNextHearingCourtSetPage.radioLabelSelector('true').click()
+    courtCaseNextHearingCourtSetPage.continueButton().click()
+
+    const courtCaseNextHearingAnswersPage = Page.verifyOnPage(CourtCaseCheckNextHearingAnswersPage)
+    courtCaseNextHearingAnswersPage
+      .summaryList()
+      .getSummaryList()
+      .should('deep.equal', {
+        'Next hearing date': futureDate.format('DD/MM/YYYY'),
+        'Next hearing location': 'Accrington Youth Court',
+        'Next hearing type': 'Court appearance',
+      })
+    courtCaseNextHearingAnswersPage.continueButton().click()
+
+    courtCaseTaskListPage = Page.verifyOnPageTitle(CourtCaseTaskListPage, 'Add a court case')
+    courtCaseTaskListPage
+      .taskList()
+      .getTaskList()
+      .should('deep.equal', [
+        {
+          name: 'Add appearance information',
+          status: 'Completed',
+        },
+        {
+          name: 'Add offences',
+          status: 'Completed',
+        },
+        {
+          name: 'Next court appearance',
+          status: 'Completed',
+        },
+        {
+          name: 'Upload court documents',
+          status: 'Optional',
+        },
+      ])
+    courtCaseTaskListPage.continueButton().click()
+
+    cy.task('verifyCreateCourtCaseRequest', { nextHearingDate: futureDate.format('YYYY-MM-DD') }).should('equal', 1)
+    Page.verifyOnPageTitle(CourtCaseConfirmationPage, 'Court case')
   })
 
-  it.skip('fill in sentencing journey', () => {
+  it('fill in sentencing journey', () => {
     cy.task('stubGetSentenceTypesByIds', [
       {
         sentenceTypeUuid: '467e2fa8-fce1-41a4-8110-b378c727eed3',

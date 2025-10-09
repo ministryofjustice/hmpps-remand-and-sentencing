45fadb18507485bd6347ef205ed82ab4e4dfd76a
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-10-09 13:01:38 +0100

    clean up

--- integration_tests/pages/sentencingCorrectManyPeriodLengthPage.ts
+++ integration_tests/pages/sentencingCorrectManyPeriodLengthPage.ts
@@ -1,15 +1,7 @@
-import Page, { PageElement } from './page'
+import Page from './page'
 
 export default class SentencingCorrectManyPeriodLengthPage extends Page {
   constructor(periodType: string) {
     super(`Select the correct ${periodType} for this sentence`)
   }
-
-  monthsInput = (): PageElement => cy.get('#sentenceLength-months')
-
-  yearsInput = (): PageElement => cy.get('#sentenceLength-years')
-
-  weeksInput = (): PageElement => cy.get('#sentenceLength-weeks')
-
-  daysInput = (): PageElement => cy.get('#sentenceLength-days')
 }

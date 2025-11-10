b635d7627a24985b3508f0ef6bb842a8d9c1d210
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-11-10 14:31:29 +0000

    RASS-1615 add non custodial task list model

---
+++ server/routes/data/NonCustodialTaskListModel.ts
@@ -0,0 +1,91 @@
+import type { CourtAppearance, TaskListItemStatus } from 'models'
+import TaskListModel from './TaskListModel'
+
+export default class NonCustodialTaskListModel extends TaskListModel {
+  constructor(
+    nomsId: string,
+    addOrEditCourtCase: string,
+    addOrEditCourtAppearance: string,
+    courtCaseReference: string,
+    appearanceReference: string,
+    courtAppearance: CourtAppearance,
+    caseReferenceSet: boolean,
+  ) {
+    super(nomsId, addOrEditCourtCase, addOrEditCourtAppearance, courtCaseReference, appearanceReference)
+    this.items = [
+      this.getAppearanceInformationItem(courtAppearance, caseReferenceSet),
+      this.getOffenceSentencesItem(courtAppearance),
+      this.getCourtDocumentsItem(courtAppearance),
+    ]
+  }
+
+  allAppearanceInformationFilledOut(courtAppearance: CourtAppearance): boolean {
+    return (
+      courtAppearance.warrantDate &&
+      courtAppearance.courtCode &&
+      courtAppearance.appearanceOutcomeUuid &&
+      courtAppearance.caseOutcomeAppliedAll !== undefined &&
+      courtAppearance.appearanceInformationAccepted
+    )
+  }
+
+  anyAppearanceInformationFilledOut(courtAppearance: CourtAppearance): boolean {
+    return (
+      courtAppearance.caseReferenceNumber !== undefined ||
+      courtAppearance.warrantDate !== undefined ||
+      courtAppearance.courtCode !== undefined ||
+      courtAppearance.appearanceOutcomeUuid !== undefined ||
+      courtAppearance.caseOutcomeAppliedAll !== undefined ||
+      courtAppearance.appearanceInformationAccepted
+    )
+  }
+
+  getOffenceSentenceTitleText(): string {
+    if (this.isAddCourtCase()) {
+      return 'Add offences'
+    }
+
+    return 'Review offences'
+  }
+
+  getOffenceSentenceHref(courtAppearance: CourtAppearance): string {
+    let href
+    if (this.allAppearanceInformationFilledOut(courtAppearance)) {
+      if (courtAppearance.offences.length === 0) {
+        href = `/person/${this.nomsId}/${this.addOrEditCourtCase}/${this.courtCaseReference}/${this.addOrEditCourtAppearance}/${this.appearanceReference}/offences/0/add-another-offence`
+      } else {
+        href = `/person/${this.nomsId}/${this.addOrEditCourtCase}/${this.courtCaseReference}/${this.addOrEditCourtAppearance}/${this.appearanceReference}/review-offences`
+      }
+    }
+    return href
+  }
+
+  getOffenceSentenceStatus(courtAppearance: CourtAppearance): TaskListItemStatus {
+    const appearanceInfoComplete = this.allAppearanceInformationFilledOut(courtAppearance)
+    if (!appearanceInfoComplete) {
+      return {
+        text: 'Cannot start yet',
+        classes: 'govuk-task-list__status--cannot-start-yet',
+      }
+    }
+    if (courtAppearance.offenceSentenceAccepted) {
+      return {
+        text: 'Completed',
+      }
+    }
+    return {
+      tag: {
+        text: 'Incomplete',
+        classes: 'govuk-tag--blue',
+      },
+    }
+  }
+
+  getCourtDocumentsHref(courtAppearance: CourtAppearance): string {
+    let href
+    if (this.allAppearanceInformationFilledOut(courtAppearance)) {
+      href = `/person/${this.nomsId}/${this.addOrEditCourtCase}/${this.courtCaseReference}/${this.addOrEditCourtAppearance}/${this.appearanceReference}/upload-court-documents`
+    }
+    return href
+  }
+}

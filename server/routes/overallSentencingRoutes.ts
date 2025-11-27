5d36bddfc4432d07c9e7e77ed3243dd6dbd3e5c4
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2025-11-27 11:14:50 +0000

    RASS-1692: small refactor

--- server/routes/overallSentencingRoutes.ts
+++ server/routes/overallSentencingRoutes.ts
@@ -272,7 +272,7 @@ export default class OverallSentencingRoutes extends BaseRoutes {
 
     if (backTo) {
       backLink = buildReturnUrlFromKey(
-        'sentencingCourtAppearance',
+        backLink,
         nomsId,
         addOrEditCourtCase,
         courtCaseReference,

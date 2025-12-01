331c803f40eb73f4c4b740e27524148adb68ecf5
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2025-12-01 10:50:26 +0000

    RASS-1672 show appearance panel in repeat journey (#1197)

--- server/routes/courtCaseRoutes.ts
+++ server/routes/courtCaseRoutes.ts
@@ -1091,7 +1091,9 @@ export default class CourtCaseRoutes extends BaseRoutes {
       legacyCaseOutcome,
       backTo,
       submitToCheckAnswers,
-      showAppearanceDetails: this.isEditJourney(addOrEditCourtCase, addOrEditCourtAppearance),
+      showAppearanceDetails:
+        this.isEditJourney(addOrEditCourtCase, addOrEditCourtAppearance) ||
+        this.isRepeatJourney(addOrEditCourtCase, addOrEditCourtAppearance),
     })
   }
 

e4a4a51d9f416069999dbc1fb01cf1e1fd438f82
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2025-12-03 15:56:00 +0000

    RASS-1708: small change

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,15 +3,15 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // Retained the configuration for packages that did not change versions:
-    "node_modules/@parcel/watcher@2.5.1": "ALLOW",
-    "node_modules/dtrace-provider@0.8.8": "ALLOW",
-    "node_modules/fsevents@2.3.3": "FORBID",
-    "node_modules/unrs-resolver@1.11.1": "ALLOW",
+    'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
+    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
+    'node_modules/fsevents@2.3.3': 'FORBID',
+    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
 
     // UPDATED: cypress@15.7.1 (Replaced 15.7.0)
-    "node_modules/cypress@15.7.1": "ALLOW",
+    'node_modules/cypress@15.7.1': 'ALLOW',
 
     // UPDATED: esbuild@0.25.12 (Replaced 0.25.8). This script is required to install the binary.
-    "node_modules/esbuild@0.25.12": "ALLOW",
+    'node_modules/esbuild@0.25.12': 'ALLOW',
   },
 })

7cd403ecea7c04dec284663688ce464ef3c070c0
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2025-12-04 10:57:53 +0000

    RASS-1708: updating workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
 export default hmppsConfig({
-  extraIgnorePaths: ['assets/js/*.js', 'esbuild/*.js'],
   extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs'],
+  extraIgnorePaths: ['assets/js/*.js', 'esbuild/*.js'],
 })

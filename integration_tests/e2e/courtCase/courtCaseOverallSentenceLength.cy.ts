6f2ab03a68962a695877e3c40919386b8c46ba77
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2025-12-29 18:04:43 +0000

    RASS-1470-tests removed regarding back link

--- integration_tests/e2e/courtCase/courtCaseOverallSentenceLength.cy.ts
+++ integration_tests/e2e/courtCase/courtCaseOverallSentenceLength.cy.ts
@@ -49,21 +49,4 @@ context('Add Court Case Sentence Length Page', () => {
       .trimTextContent()
       .should('equal', 'There is a problem The sentence length cannot be 0')
   })
-
-  it('Navigating away from the page and coming back using the back-link retains the state of the page', () => {
-    courtCaseOverallSentenceLengthPage.radioLabelSelector('true').click()
-    courtCaseOverallSentenceLengthPage.yearsInput().type('2')
-    courtCaseOverallSentenceLengthPage.continueButton().click()
-    offenceConvictionDatePage = Page.verifyOnPageTitle(
-      OffenceConvictionDatePage,
-      'Is the conviction date the same for all offences on the warrant?',
-    )
-    offenceConvictionDatePage.backLink().click()
-    courtCaseOverallSentenceLengthPage.radioSelector('true').should('be.checked')
-    courtCaseOverallSentenceLengthPage.yearsInput().should('have.value', '2')
-    courtCaseOverallSentenceLengthPage.radioLabelSelector('false').click()
-    courtCaseOverallSentenceLengthPage.continueButton().click()
-    offenceConvictionDatePage.backLink().click()
-    courtCaseOverallSentenceLengthPage.radioSelector('false').should('be.checked')
-  })
 })

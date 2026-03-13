bb9a8515502d3f652d8796fca9ae11c6c5f77132
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-03-13 16:12:36 +0000

    RASS-1923 remove double sort from start page

--- server/routes/data/CourtCasesDetailsModel.ts
+++ server/routes/data/CourtCasesDetailsModel.ts
@@ -9,7 +9,7 @@ import {
 } from '../../@types/remandAndSentencingApi/remandAndSentencingClientTypes'
 import config from '../../config'
 import { pagedAppearancePeriodLengthToSentenceLength, pagedChargeToOffence } from '../../utils/mappingUtils'
-import { orderOffences, sortByDateDesc } from '../../utils/utils'
+import { sortByDateDesc } from '../../utils/utils'
 
 export default class CourtCasesDetailsModel {
   courtCaseUuid: string
@@ -104,7 +104,7 @@ export default class CourtCasesDetailsModel {
         return sortByDateDesc(b.createdAt, a.createdAt)
       })
       .slice(0, 6)
-    this.offences = orderOffences(charges?.map(charge => pagedChargeToOffence(charge)))
+    this.offences = charges?.map(charge => pagedChargeToOffence(charge))
     this.sentenceTypeMap = Object.fromEntries(
       charges
         ?.filter(charge => charge.sentence?.sentenceType)

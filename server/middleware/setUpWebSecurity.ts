5d7b631e958fda773c41b0d50dcc125054ff9b5c
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-05-15 12:25:40 +0100

    fix

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -21,8 +21,14 @@ export default function setUpWebSecurity(): Router {
   // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
   // This ensures only scripts we trust are loaded, and not anything injected into the
   // page by an attacker.
-  const scriptSrc = ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`]
-  const styleSrc = ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`]
+  const scriptSrc = [
+    "'self'",
+    (_req: IncomingMessage, res: ServerResponse) => `'nonce-${(res as Response).locals.cspNonce}'`,
+  ]
+  const styleSrc = [
+    "'self'",
+    (_req: IncomingMessage, res: ServerResponse) => `'nonce-${(res as Response).locals.cspNonce}'`,
+  ]
   const fontSrc = ["'self'"]
   const imgSrc = ["'self'", 'data:']
   const formAction = [

d80c9973fdc3434f625634149ce4ce2fdbc0942b
Carlo Veo <144013256+carloveo-moj@users.noreply.github.com>
2026-06-09 11:20:31 +0100

    RASS-2304 supress new hearing and delete links when court case is merged (#1545)

--- integration_tests/e2e/other/start.cy.ts
+++ integration_tests/e2e/other/start.cy.ts
@@ -213,6 +213,7 @@ context('Start Page', () => {
     startPage
       .mergedToCaseInset('c0f90a3c-f1c5-4e2e-9360-2a9d7bd33dda')
       .should('contain.text', 'This court case was merged on 25/02/2024 with NOMIS897 at Accrington Youth Court')
+    startPage.addAppearanceLink('c0f90a3c-f1c5-4e2e-9360-2a9d7bd33dda').should('not.exist')
   })
 
   it('display person not found page when prisoner number does not exist', () => {

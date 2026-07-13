c9c80a796978de8cb5af3d213eaebde8ab0ef838
Carlo Veo <144013256+carloveo-moj@users.noreply.github.com>
2026-07-13 14:57:41 +0100

    RASS-2375 add delete breach document page (#1585)

---
+++ integration_tests/e2e/allFeaturesOn/breachDeleteDocument.cy.ts
@@ -0,0 +1,38 @@
+import Page from '../../pages/page'
+import UploadBreachOrderPage from '../../pages/UploadBreachOrderPage'
+import ViewBreachOrderPage from '../../pages/ViewBreachOrderPage'
+import BreachDeleteDocumentPage from '../../pages/BreachDeleteDocumentPage'
+
+context('Remove document confirmation page', () => {
+  let breachDeleteDocumentPage: BreachDeleteDocumentPage
+  beforeEach(() => {
+    cy.task('happyPathStubs')
+    cy.task('stubUploadTempDocument', {
+      type: 'BREACH_ORDER',
+    })
+    cy.task('stubUploadDocument')
+    cy.signIn()
+    cy.visit('/person/A1234AB/edit-court-case/0/add-court-appearance/0/breach/upload-breach-order')
+    const uploadBreachOrderPage = Page.verifyOnPage(UploadBreachOrderPage)
+    uploadBreachOrderPage.fileInput().selectFile('cypress/fixtures/testfile.doc')
+    uploadBreachOrderPage.continueButton().click()
+    cy.location('pathname').should('include', '/view-breach-order')
+    const viewBreachOrderPage = Page.verifyOnPage(ViewBreachOrderPage)
+    viewBreachOrderPage.removeDocumentLink('BREACH_ORDER').click()
+    breachDeleteDocumentPage = Page.verifyOnPage(BreachDeleteDocumentPage)
+  })
+
+  it('submitting without selecting anything results in error', () => {
+    breachDeleteDocumentPage.continueButton().click()
+    breachDeleteDocumentPage
+      .errorSummary()
+      .trimTextContent()
+      .should('equal', 'There is a problem You must select an option.')
+  })
+
+  it('after confirm and continue the upload breach order page should appear', () => {
+    breachDeleteDocumentPage.radioLabelSelector('true').click()
+    breachDeleteDocumentPage.continueButton().click()
+    Page.verifyOnPage(UploadBreachOrderPage)
+  })
+})

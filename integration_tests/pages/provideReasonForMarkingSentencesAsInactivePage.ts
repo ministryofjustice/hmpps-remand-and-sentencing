82cbb213f243a6167977fecc837a5722cedea69c
Md Hasan Rahman <hassan.rahman@justice.gov.uk>
2026-09-01 08:52:00 +0100

    RASS-2573: Add integration tests, pages, and route logic for marking sentences as inactive, including validation for active consecutive sentences (#1688)
    * RASS-2573: Add integration tests, pages, and route logic for marking sentences as inactive, including validation for active consecutive sentences
    
    * RASS-2573: Remove redundant comments in sentencingRoutes to improve code readability

---
+++ integration_tests/pages/provideReasonForMarkingSentencesAsInactivePage.ts
@@ -0,0 +1,7 @@
+import Page from './page'
+
+export default class ProvideReasonForMarkingSentencesAsInactivePage extends Page {
+  constructor() {
+    super('Provide a reason for marking sentences as inactive')
+  }
+}
